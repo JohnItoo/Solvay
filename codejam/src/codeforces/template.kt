2be3b52fb8c5b6d2caea7a9dc82af14fde481aef
JohnOhue <itoohue@gmail.com>
2020-05-29 17:00:00 +0100

    2020-05-29

--- codejam/src/codeforces/template.kt
+++ codejam/src/codeforces/template.kt
@@ -18,7 +18,7 @@ fun main() {
     val outputStream = System.out
     val `in` = Main.InputReader(inputStream)
     val out = PrintWriter(outputStream)
-    val solver = Main.TaskA()
+    val solver = Main.TaskB()
     solver.solve(1, `in`, out)
     out.close()
 }
@@ -30,7 +30,7 @@ object Main {
         val outputStream = System.out
         val `in` = InputReader(inputStream)
         val out = PrintWriter(outputStream)
-        val solver = TaskA()
+        val solver = TaskB()
         solver.solve(1, `in`, out)
         out.close()
     }
@@ -53,6 +53,55 @@ object Main {
 
     }
 
+    internal class TaskB {
+        fun solve(testNumber: Int, `in`: InputReader, out: PrintWriter) {
+            val n = `in`.nextInt()
+
+            for (i in 0 until n) {
+                val a = `in`.nextInt()
+                val b = `in`.nextInt()
+                val c = `in`.nextInt()
+                val s = `in`.next()
+                out.println(a)
+                var ans = 0
+//                if(s[0] == '1') {ans += b}
+                var sings = 0
+                var st = 0
+                var j = 0
+                out.println(s)
+                while (j < a) {
+                    4
+                    4 5 7
+                    1011
+                    4 4 10
+                    0101
+                    5 3 4
+                    11011
+                    6 4 6
+                    011101
+                    if ((j == a - 1 && s[j] == '1') || s[j] == '1' && s[j + 1] == '0') {
+                        ans += b
+                        j++
+
+                    }
+//                    else {
+//                        var start = j
+//                        while (j < a && s[j] != '0') {
+//                            j++
+//                        }
+//                        var end = j
+//                        var num = end - start
+//                        var red = num / 2
+//                        var rem = num - red
+//                        ans += (b * rem) + ((c - b) * red)
+//                    }
+                }
+                out.println(ans)
+            }
+
+        }
+    }
+
     internal class TaskA {
         fun solve(testNumber: Int, `in`: InputReader, out: PrintWriter) {
             val n = `in`.nextInt()
@@ -60,11 +109,11 @@ object Main {
             for (i in 0 until n) {
                 val a = `in`.nextInt()
                 val b = `in`.nextInt()
-               var w = 0
+                var w = 0
                 var x = 0
                 var y = 0
                 var z = 0
-                while((w + x + y + z) != a) {
+                while ((w + x + y + z) != a) {
                     w += 1
                     x = w * b
                     y = x * b
@@ -75,7 +124,6 @@ object Main {
         }
 
     }
-
 
 
     internal class InputReader(stream: InputStream) {
