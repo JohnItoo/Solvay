a76ccd8071ea9abeddd21ea5725e07d61908be24
JohnOhue <itoohue@gmail.com>
2020-06-06 01:00:00 +0100

    2020-06-06

---
+++ cf/MostafasLadder/CF682-D2-A.cpp
@@ -0,0 +1,78 @@
+//============================================================================
+// Name        : template.cpp
+// Author      :
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+#include <algorithm>
+#include <set>
+#include <map>
+#include <set>
+#include <vector>
+#include <string.h> // for memset in CF judge.
+using namespace std;
+#define _CRT_SECURE_NO_DEPRECATE // suppress some compilation warning messages (for VC++ users)
+// Shortcuts for "common" data types in contests
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int, int> ii;
+typedef vector<ii> vii;
+typedef set<int> si;
+typedef map<string, int> msi;
+// To simplify repetitions/loops, Note: define your loop style and stick with it!
+#define x first
+#define y second
+#define pb push_back
+#define mp make_pair
+#define REP(i, a, b) \
+for (int i = int(a); i <= int(b); i++) // a to b, and variable i is local!
+#define forn(i,n) \
+for (int i =0; i<(n); i++)
+#define TRvi(c, it) \
+for (vi::iterator it = (c).begin(); it != (c).end(); it++)
+#define TRvii(c, it) \
+for (vii::iterator it = (c).begin(); it != (c).end(); it++)
+#define TRmsi(c, it) \
+for (msi::iterator it = (c).begin(); it != (c).end(); it++)
+#define INF 2000000000 // 2 billion
+// If you need to recall how to use memset:
+#define MEMSET_INF 127 // about 2B
+#define MEMSET_HALF_INF 63 // about 1B
+//memset(dist, MEMSET_INF, sizeof dist); // useful to initialize shortest path distances
+//memset(dp_memo, -1, sizeof dp_memo); // useful to initialize DP memoization table
+//memset(arr, 0, sizeof arr); // useful to clear array of integers
+// to_string(int)
+
+string trUpp(string s) {
+
+	transform(s.begin(), s.end(), s.begin(), ::toupper);
+	return "";
+}
+
+int main() {
+	int n, m; cin >> n >> m;
+	int mn = min(n, m);
+	int mx = max(n, m);
+	int ans = 0;
+	int dl = (mx + 4) / 5;
+
+	REP(i, 1, mn) {
+		int fac = ( i + 4) / 5;
+		// int gg = 5 * fac;
+		// while (gg - i <= mx) {
+		// 	// cout << i << " " <<  gg - i << endl;
+		// 	if (gg - i > 0) ans ++;
+		// 	gg += 5;
+
+		// }
+		if((5*dl) - i <= mx) ans += dl;
+		else ans += dl-1;
+	}
+	cout << ans << endl;
+
+
+	return 0;
+}
