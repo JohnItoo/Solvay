1df79f9a4a2be46ede47056de95c5553258048da
JohnOhue <itoohue@gmail.com>
2020-06-07 11:00:05 +0100

    2020-06-07

--- cf/MostafasLadder/CF382-D2-A.cpp
+++ cf/MostafasLadder/CF382-D2-A.cpp
@@ -1,6 +1,6 @@
 //============================================================================
 // Name        : template.cpp
-// Author      : 
+// Author      :
 // Version     :
 // Copyright   : Your copyright notice
 // Description : Hello World in C++, Ansi-style
@@ -44,62 +44,62 @@ for (msi::iterator it = (c).begin(); it != (c).end(); it++)
 //memset(dist, MEMSET_INF, sizeof dist); // useful to initialize shortest path distances
 //memset(dp_memo, -1, sizeof dp_memo); // useful to initialize DP memoization table
 //memset(arr, 0, sizeof arr); // useful to clear array of integers
-	// to_string(int)
+// to_string(int)
 
 string trUpp(string s) {
-	
-  	transform(s.begin(), s.end(), s.begin(), ::toupper);
- return "";
+
+	transform(s.begin(), s.end(), s.begin(), ::toupper);
+	return "";
 }
 
 int main() {
-	string s; 
+	string s;
 	cin >> s;
 	string q ; cin >> q;
 	ll i = 0;
 	ll l = 0;
 	string lef = "";
-	while( s[i] != '|') {
+	while ( s[i] != '|') {
 		lef.pb(s[i]);
 		l++;
 		i++;
 	}
 
-	ll r = s.length()- (l+1);
-	string rig = s.substr(i+1, r);
+	ll r = s.length() - (l + 1);
+	string rig = s.substr(i + 1, r);
 	int sh = 0;
-	if(l < r) {
-		while(l < r && sh < q.length()) {
+	if (l < r) {
+		while (l < r && sh < q.length()) {
 			l++;
-            lef.pb(q[sh]);
-            sh++;
+			lef.pb(q[sh]);
+			sh++;
 		}
 	} else {
-		while(r < l && sh < q.length()) {
+		while (r < l && sh < q.length()) {
 			r++;
 			rig.pb(q[sh]);
 			sh++;
 		}
 	}
-	if(l == r) {
+	if (l == r) {
 		int lefid = 1;
-		while(sh < q.length()) {
-           if(lefid == 1) {
-           	lef.pb(q[sh]);
-             lefid = 0;
-           } else {
-           	rig.pb(q[sh]);
-           	lefid = 1;
-           } 
-           sh++;
+		while (sh < q.length()) {
+			if (lefid == 1) {
+				lef.pb(q[sh]);
+				lefid = 0;
+			} else {
+				rig.pb(q[sh]);
+				lefid = 1;
+			}
+			sh++;
 		}
-		if(lef.length() == rig.length()) {
-			cout << lef << "|"<< rig << endl;
+		if (lef.length() == rig.length()) {
+			cout << lef << "|" << rig << endl;
 		} else {
 			cout << "Impossible";
 		}
 	} else {
 		cout << "Impossible";
 	}
-return 0;
+	return 0;
 }
