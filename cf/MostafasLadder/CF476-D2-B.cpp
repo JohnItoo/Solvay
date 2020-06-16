bb4801fb930544fc62c8d088dc0d5ab8a3f8a554
JohnOhue <itoohue@gmail.com>
2020-06-16 13:00:00 +0100

    2020-06-16

--- cf/MostafasLadder/CF476-D2-B.cpp
+++ cf/MostafasLadder/CF476-D2-B.cpp
@@ -1,6 +1,6 @@
 //============================================================================
 // Name        : template.cpp
-// Author      : 
+// Author      :
 // Version     :
 // Copyright   : Your copyright notice
 // Description : Hello World in C++, Ansi-style
@@ -44,37 +44,37 @@ for (msi::iterator it = (c).begin(); it != (c).end(); it++)
 //memset(dist, MEMSET_INF, sizeof dist); // useful to initialize shortest path distances
 //memset(dp_memo, -1, sizeof dp_memo); // useful to initialize DP memoization table
 //memset(arr, 0, sizeof arr); // useful to clear array of integers
-	// to_string(int)
+// to_string(int)
 
 string trUpp(string s) {
-	
-  	transform(s.begin(), s.end(), s.begin(), ::toupper);
- return "";
+
+	transform(s.begin(), s.end(), s.begin(), ::toupper);
+	return "";
 }
 
 int main() {
 	string s; string t;
 	cin >> s >> t;
 	int n = (s.length() * 2)  + 2;
-	float g1[s.length()+1][n];
-	float g2[s.length()+1][n];
+	float g1[s.length() + 1][n];
+	float g2[s.length() + 1][n];
 	memset(g1, (float) 0 , sizeof g1);
 	memset(g2, (float) 0 , sizeof g2);
 
-    int start = s.length()+1;
-    int end = 0;
+	int start = s.length() + 1;
+	int end = 0;
 	forn(i, s.length()) {
-       if(s[i] == '+') {
-         g1[i+1][start+1] = (float) 1;
-         start += 1;
-       } else {
-         g1[i+1][start-1] = (float) 1;
-         start -= 1;
-       }
-       if(i == s.length() -1) end = start;
+		if (s[i] == '+') {
+			g1[i + 1][start + 1] = (float) 1;
+			start += 1;
+		} else {
+			g1[i + 1][start - 1] = (float) 1;
+			start -= 1;
+		}
+		if (i == s.length() - 1) end = start;
 	}
 	REP(i, 1, s.length()) {
-		REP(j, 1, n-1) {
+		REP(j, 1, n - 1) {
 			cout << g1[i][j] << " ";
 		}
 		cout << endl;
@@ -82,33 +82,43 @@ int main() {
 	cout << endl;
 
 
-    start = s.length()+1;
-    float curr = (float) 1;
+	start = s.length() + 1;
+	vi idxs;
+	float curr = (float) 1;
 	forn(i, s.length()) {
-      if(t[i] == '+') {
-      	cout << " + ";
-      	g2[i+1][start+1] = curr;
-      	start += 1;
-      } else if(t[i] == '-') {
-      	cout << " - ";
-      	 g2[i+1][start-1] = curr;
-         start -= 1;
-      } else {
-      	cout << " ? ";
-      	 curr /= (float) 2;
-      	 g2[i+1][start+1] = curr;
-      	 g2[i+1][start-1] = curr;
-      }
+		if (t[i] == '+') {
+			cout << " + ";
+			g2[i + 1][start + 1] = 1;
+			start += 1;
+		} else if (t[i] == '-') {
+			cout << " - ";
+			g2[i + 1][start - 1] = 1;
+			start -= 1;
+		} else {
+			cout << " ? ";
+			if (idxs.size() == 0) {
+				g2[i + 1][start + 1] = 1;
+				g2[i + 1][start - 1] =  1;
+			} else {
+				for (int position : idxs) {
+					g2[i + 1][position - 1] = 1;
+					g2[i - 1][position - 1] = 1;
+				}
+			}
+			curr /= (float) 2;
+
+
+		}
 	}
 	cout << endl;
-		REP(i, 1, s.length()) {
-		REP(j, 1, n-1) {
+	REP(i, 1, s.length()) {
+		REP(j, 1, n - 1) {
 			cout << g2[i][j] << " ";
 		}
 		cout << endl;
 	}
 
 	cout << min(g1[s.length()][end], g2[s.length()][end]) << endl;
-	
-return 0;
+
+	return 0;
 }
