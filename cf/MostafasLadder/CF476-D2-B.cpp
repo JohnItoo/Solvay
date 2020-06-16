dda672ea5cd80577cce96aafb603747cbd6ecd42
JohnOhue <itoohue@gmail.com>
2020-06-16 11:00:00 +0100

    2020-06-16

--- cf/MostafasLadder/CF476-D2-B.cpp
+++ cf/MostafasLadder/CF476-D2-B.cpp
@@ -53,6 +53,62 @@ string trUpp(string s) {
 }
 
 int main() {
+	string s; string t;
+	cin >> s >> t;
+	int n = (s.length() * 2)  + 2;
+	float g1[s.length()+1][n];
+	float g2[s.length()+1][n];
+	memset(g1, (float) 0 , sizeof g1);
+	memset(g2, (float) 0 , sizeof g2);
+
+    int start = s.length()+1;
+    int end = 0;
+	forn(i, s.length()) {
+       if(s[i] == '+') {
+         g1[i+1][start+1] = (float) 1;
+         start += 1;
+       } else {
+         g1[i+1][start-1] = (float) 1;
+         start -= 1;
+       }
+       if(i == s.length() -1) end = start;
+	}
+	REP(i, 1, s.length()) {
+		REP(j, 1, n-1) {
+			cout << g1[i][j] << " ";
+		}
+		cout << endl;
+	}
+	cout << endl;
+
+
+    start = s.length()+1;
+    float curr = (float) 1;
+	forn(i, s.length()) {
+      if(t[i] == '+') {
+      	cout << " + ";
+      	g2[i+1][start+1] = curr;
+      	start += 1;
+      } else if(t[i] == '-') {
+      	cout << " - ";
+      	 g2[i+1][start-1] = curr;
+         start -= 1;
+      } else {
+      	cout << " ? ";
+      	 curr /= (float) 2;
+      	 g2[i+1][start+1] = curr;
+      	 g2[i+1][start-1] = curr;
+      }
+	}
+	cout << endl;
+		REP(i, 1, s.length()) {
+		REP(j, 1, n-1) {
+			cout << g2[i][j] << " ";
+		}
+		cout << endl;
+	}
+
+	cout << min(g1[s.length()][end], g2[s.length()][end]) << endl;
 	
 return 0;
 }
