7737156744a737896792a863a3eb4eb330de136d
JohnOhue <itoohue@gmail.com>
2020-07-08 22:00:00 +0100

    2020-07-08

--- cf/training/CF-1249B1.cpp
+++ cf/training/CF-1249B1.cpp
@@ -1,6 +1,6 @@
 //============================================================================
 // Name        : template.cpp
-// Author      : 
+// Author      :
 // Version     :
 // Copyright   : Your copyright notice
 // Description : Hello World in C++, Ansi-style
@@ -46,7 +46,33 @@ for (msi::iterator it = (c).begin(); it != (c).end(); it++)
 //memset(arr, 0, sizeof arr); // useful to clear array of integers
 
 int main() {
-  ios::sync_with_stdio(false);
-  cin.tie(0);
-return 0;
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	int tc;
+	cin >> tc;
+	while (tc--) {
+		int n; cin >> n;
+		int p[n + 1];
+		REP(i, 1, n) {
+			int x; cin >> x;
+			p[i] = x;
+		}
+		int res[n + 1];
+		REP(i, 1, n) {
+			int ct = 1;
+			int k = i;
+
+			while (p[k] != i) {
+				k = p[k];
+				ct++;
+			}
+			res[i] = ct;
+		}
+
+		REP(i, 1, n) {
+			cout << res[i] << " ";
+		}
+		cout << "\n";
+	}
+	return 0;
 }
