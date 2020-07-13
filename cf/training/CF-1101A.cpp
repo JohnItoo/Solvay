3bbbf00f88afa751e4e047a691cf63d857be7822
JohnOhue <itoohue@gmail.com>
2020-07-13 21:00:00 +0100

    2020-07-13

--- cf/training/CF-1101A.cpp
+++ cf/training/CF-1101A.cpp
@@ -1,6 +1,6 @@
 //============================================================================
 // Name        : template.cpp
-// Author      : 
+// Author      :
 // Version     :
 // Copyright   : Your copyright notice
 // Description : Hello World in C++, Ansi-style
@@ -46,7 +46,17 @@ for (msi::iterator it = (c).begin(); it != (c).end(); it++)
 //memset(arr, 0, sizeof arr); // useful to clear array of integers
 
 int main() {
-  ios::sync_with_stdio(false);
-  cin.tie(0);
-return 0;
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	int tc; cin >> tc;
+	while (tc--) {
+		ll l, r, d; cin >> l >> r >> d;
+		ll div = r / d;
+		div += 1;
+		ll ans = 0;
+		if (l > d) ans = d;
+		else ans = d * div;
+		cout << ans << "\n";
+	}
+	return 0;
 }
