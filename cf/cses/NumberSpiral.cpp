79c4dd0635ec13c778160662939b4b10fe80ce32
JohnOhue <itoohue@gmail.com>
2020-07-20 14:00:00 +0100

    2020-07-20

--- cf/cses/NumberSpiral.cpp
+++ cf/cses/NumberSpiral.cpp
@@ -7,6 +7,7 @@ int main() {
 		long long x, y;
 		cin >> x >> y;
 		long long sq = max(x, y);
+		if((sq & 1) == 0) sq += 1;
 		vector<vector<long long>> grid(sq + 1, vector<long long>(sq + 1, 0));
 
 		grid[0][0] = 1;
@@ -14,78 +15,111 @@ int main() {
 		int a[4][2] = { {1, 0}, {0, 1}, { -1, 0}, {0, -1}}; //down, right, up, left
 		long long done = 1;
 		long long count = 1;
-		long long i = 1; long long j = 2s;
+		long long i = 1; long long j = 2;
 		long long last = 1;
 		while (last < sq * sq) {
-			while (grid[i - 1][j] != 0 && j <= sq) {
+			// while (grid[i - 1][j] != 0 && j <= sq) {
+			// 	last += 1;
+			// 	grid[i][j] = last;
+			// 	j += 1;
+			// }
+
+			if (i > sq  || i < 1 || j > sq || j < 1) break;
+			last += 1;
+			grid[i][j] = last;
+
+			cout << "last " << last << "\n";
+             
+             while(i+1 <= sq && grid[i+1][j-1] != 0) {
+             	last += 1;
+             	i += 1;
+             	grid[i][j] = last;
+             }
+
+//
+			last += 1;
+			i += 1;
+			if (i > sq  || i < 1 || j > sq || j < 1) break;
+
+			grid[i][j] = last;
+			j -= 1;
+
+
+
+			// if (i > sq  || i < 1 || j > sq || j < 1) break;
+
+			// last += 1;
+			// grid[i][j] = last;
+
+
+			while (j  > 1 && last < (sq * sq)) {
 				last += 1;
 				grid[i][j] = last;
-				j+=1;
+				j -= 1;
 			}
 
-			if (i > sq  || i < 1 || j > sq || j < 1) break;
 			last += 1;
 			grid[i][j] = last;
 
-
 			last += 1;
-			i += a[0][0];
-			j += a[0][1];
-			if (i > sq  || i < 1 || j > sq || j < 1) break;
-
+			i += 1;
 			grid[i][j] = last;
 
-			cout << "here\n";
+			cout << "here " << last << "\n";
 			while (last < (sq * sq ) && j <= sq && grid[i - 1][j] != 0) {
 				last += 1;
-				i += a[1][0];
-				j += a[1][1];
+				j += 1;
 				grid[i][j] = last;
 			}
 
-			if (i > sq  || i < 1 || j > sq || j < 1) break;
-
 			last += 1;
+			i -= 1;
 			grid[i][j] = last;
 
-
+            cout <<" New last " << last << "\n";
 			while (last < (sq * sq) && i > 1) {
 				last += 1;
 				i -= 1;
 				grid[i][j] = last;
 			}
 
-
-			j += 1;
-			last += 1;
-
-			if (i > sq  || i < 1 || j > sq || j < 1) break;
-
-
+		
+			if(last + 1 > sq * sq) break;
+			j+=1;
 			grid[i][j] = last;
 
-			while (last < (sq * sq) && i <= sq && grid[i][j - 1] != 0 ) {
-				last += 1;
-				i += 1;
-				grid[i][j] = last;
+			// j += 1;
+			// last += 1;
+
+			// if (i > sq  || i < 1 || j > sq || j < 1) break;
+
+
+			// grid[i][j] = last;
+
+			// while (last < (sq * sq) && i <= sq && grid[i][j - 1] != 0 ) {
+			// 	last += 1;
+			// 	i += 1;
+			// 	grid[i][j] = last;
+			// }
+
+			// last += 1;
+			// grid[i][j] = last;
+
+
+
+			// if (i > sq  || i < 1 || j > sq || j < 1) break;
+
+
+			// last += 1;
+			// i += 1;
+			// grid[i][j] = last;
+
+		}
+		for (int i = 0; i <= sq; i++) {
+			for (int j = 1; j <= sq; j++) {
+				cout << grid[i][j] << " ";
 			}
-
-			last += 1;
-			grid[i][j] = last;
-
-			while (j  > 1 && last < (sq * sq)) {
-				j -= 1;
-				last += 1;
-				grid[i][j] = last;
-			}
-
-			if (i > sq  || i < 1 || j > sq || j < 1) break;
-
-
-			last += 1;
-			i += 1;
-			grid[i][j] = last;
-
+			cout << "\n";
 		}
 		cout << grid[x][y] << "\n";
 
