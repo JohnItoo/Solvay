534e0cb822ce0a1ece189e9ecb2c66032266bfd9
JohnOhue <itoohue@gmail.com>
2020-07-21 17:00:00 +0100

    2020-07-21

---
+++ cses/BitStrings.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int mod = 10e9 + 7;
+int main() {
+	int n; 
+	cin >> n;
+	unsigned long long ans = pow(2, n);
+	// ans %= mod;
+	cout <<  ans << "\n";
+
+}
