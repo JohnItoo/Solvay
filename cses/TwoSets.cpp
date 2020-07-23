cb4aa48cec5e0ab6cac0a8a9b5df42259659b943
JohnOhue <itoohue@gmail.com>
2020-07-23 13:00:00 +0100

    2020-07-23

--- cses/TwoSets.cpp
+++ cses/TwoSets.cpp
@@ -1,53 +1,51 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// GREEDY ; Always pick max till
 int main() {
-	int n;
+	long long n;
 	cin >> n;
-	unsigned long long int sum = (n * (n + 1)) / 2;
-	cout << "This is sum " << sum << "\n";
+
+	long long sum = (n * (n + 1)) / 2;
+
+
 	if ((sum % 2 != 0 ) ) {
 		cout << "NO\n";
 	} else {
 		vector<long long> a;
 		vector<long long> b;
 		map<long long, int> marked;
- 		unsigned long long int s = sum / 2;
-      unsigned long long  int t = n;
-      unsigned long long int curr = 0;
-      unsigned long long int last = n;
-       cout << s << "\n";
-       while(t + curr <= s) {
-          a.push_back(t);
-          curr += t;
-          marked[t] = true;
-          cout << "this is t ; " << t << "\n";
-          last = t;
-          t--;
-       } 
+		long long s = sum / 2;
+		long long t = n;
+		long long curr = 0;
+		long long last = n;
+		while (t + curr <= s) {
+			a.push_back(t);
+			curr += t;
+			last = t;
+			t--;
+		}
 
-       if(s > curr) {
-       	 a.push_back(s-curr);
-       	 marked[s-curr] = 1;
-       	 cout << "this is t ; " << s - curr << "\n";
+		if (s > curr) {
+			a.push_back(s - curr);
+			marked[s - curr] = 1;
+		}
 
-       }
+		for (int i = 1; i < last; i++) {
+			if (marked.find(i) != marked.end()) continue;
+			b.push_back(i);
+		}
 
-      //  for(int i = 1; i <=n; i++) {
-      //  	if(marked[i]) continue;
-      //  	b.push_back(i);
-      //  }
-      
-      // cout << "YES\n";
-      // cout << a.size() << "\n";
-      // for(int i = 0; i < a.size(); i++) {
-      // 	cout << a[i] << " ";
-      // }
-      // cout << "\n";
+		cout << "YES\n";
+		cout << a.size() << "\n";
+		for (int i = 0; i < a.size(); i++) {
+			cout << a[i] << " ";
+		}
+		cout << "\n";
 
-      // cout << b.size() << "\n";
-      // for(int i = 0; i < b.size(); i++) {
-      // 	cout << b[i] << " ";
-      // }
+		cout << b.size() << "\n";
+		for (int i = 0; i < b.size(); i++) {
+			cout << b[i] << " ";
+		}
 	}
 }
