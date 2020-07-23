73832c0b55d45232c209e00f83b1a15f174de4e2
JohnOhue <itoohue@gmail.com>
2020-07-23 11:00:00 +0100

    2020-07-23

--- cses/TwoSets.cpp
+++ cses/TwoSets.cpp
@@ -10,48 +10,26 @@ int main() {
 	} else {
 		vector<int> a;
 		vector<int> b;
-		bool who = true;
-		int curr = 1;
-		while (curr <= n) {
-			if (who) {
-				a.push_back(curr);
-				curr += 1;
-				if (curr <= n) {
-					a.push_back(curr);
-					curr += 1;
-				}
-				if (curr  <= n) {
-					b.push_back(curr);
-					curr += 1;
-				}
-				who = false;
-			} else {
-				a.push_back(curr);
-				curr += 1;
-				if (curr  <= n) {
-					b.push_back(curr);
-					curr += 1;
-				}
-				if (curr <= n) {
-					b.push_back(curr);
-					curr += 1;
+		long long s = sum / 2;
+		int dp[s+1][n+1];
+		memset(dp, 1000000, sizeof dp);
+		dp[0][0] = 0;
 
-				}
-				who = true;
+		for(int i = 0; i <= s; i++) {
+			for(int j = 1; j <= n; j++) {
+				dp[i][j] = dp[i][j-1];
+                if(j <= i) {
+                	dp[i][j] = min(dp[i][j], dp[i-j][j-1]+1);
+                }
 			}
 		}
-		cout << "YES\n"; f
-		cout << a.size() << "\n";
-		for (int i = 0; i < a.size(); i++) {
-			cout << a[i] << " ";
-		}
-		cout << "\n";
 
-		cout << b.size() << "\n";
-		for (int i = 0; i < b.size(); i++) {
-			cout << b[i] << " ";
+		for(int i = 0; i <= s; i++) {
+			for(int j = 0; j <= n; j++) {
+				cout << dp[i][j] << " ";
+			}
+			cout << "\n";
 		}
-		cout << "\n";
 
 	}
 }
