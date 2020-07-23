b5e5e10d4265cc4986dca29963b23c6fca659f1a
JohnOhue <itoohue@gmail.com>
2020-07-23 12:00:00 +0100

    2020-07-23

--- cses/TwoSets.cpp
+++ cses/TwoSets.cpp
@@ -4,32 +4,50 @@ using namespace std;
 int main() {
 	int n;
 	cin >> n;
-	long long sum = (n * (n + 1)) / 2;
-	if ((sum & 1) ) {
+	unsigned long long int sum = (n * (n + 1)) / 2;
+	cout << "This is sum " << sum << "\n";
+	if ((sum % 2 != 0 ) ) {
 		cout << "NO\n";
 	} else {
-		vector<int> a;
-		vector<int> b;
-		long long s = sum / 2;
-		int dp[s+1][n+1];
-		memset(dp, 1000000, sizeof dp);
-		dp[0][0] = 0;
+		vector<long long> a;
+		vector<long long> b;
+		map<long long, int> marked;
+ 		unsigned long long int s = sum / 2;
+      unsigned long long  int t = n;
+      unsigned long long int curr = 0;
+      unsigned long long int last = n;
+       cout << s << "\n";
+       while(t + curr <= s) {
+          a.push_back(t);
+          curr += t;
+          marked[t] = true;
+          cout << "this is t ; " << t << "\n";
+          last = t;
+          t--;
+       } 
 
-		for(int i = 0; i <= s; i++) {
-			for(int j = 1; j <= n; j++) {
-				dp[i][j] = dp[i][j-1];
-                if(j <= i) {
-                	dp[i][j] = min(dp[i][j], dp[i-j][j-1]+1);
-                }
-			}
-		}
+       if(s > curr) {
+       	 a.push_back(s-curr);
+       	 marked[s-curr] = 1;
+       	 cout << "this is t ; " << s - curr << "\n";
 
-		for(int i = 0; i <= s; i++) {
-			for(int j = 0; j <= n; j++) {
-				cout << dp[i][j] << " ";
-			}
-			cout << "\n";
-		}
+       }
 
+      //  for(int i = 1; i <=n; i++) {
+      //  	if(marked[i]) continue;
+      //  	b.push_back(i);
+      //  }
+      
+      // cout << "YES\n";
+      // cout << a.size() << "\n";
+      // for(int i = 0; i < a.size(); i++) {
+      // 	cout << a[i] << " ";
+      // }
+      // cout << "\n";
+
+      // cout << b.size() << "\n";
+      // for(int i = 0; i < b.size(); i++) {
+      // 	cout << b[i] << " ";
+      // }
 	}
 }
