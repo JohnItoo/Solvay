99c726831d9c43a05c6b349068d2eab66741bca8
JohnOhue <itoohue@gmail.com>
2020-07-30 19:00:00 +0100

    2020-07-30

--- codeforces/training/CF-1283A.cpp
+++ codeforces/training/CF-1283A.cpp
@@ -1,6 +1,6 @@
 //============================================================================
 // Name        : template.cpp
-// Author      :
+// Author      : 
 // Version     :
 // Copyright   : Your copyright notice
 // Description : Hello World in C++, Ansi-style
@@ -40,19 +40,7 @@ for (msi::iterator it = (c).begin(); it != (c).end(); it++)
 //memset(arr, 0, sizeof arr); // useful to clear array of integers
 
 int main() {
-	ios::sync_with_stdio(false);
-	cin.tie(0);
-	int tc; cin >> tc;
-	while (tc--) {
-		int n; cin >> n;
-		int od = 0; int  evs = 0;
-		forn(i, n) {
-			int x; cin >> x;
-			if (x & 1)od++;
-			else evs++;
-		}
-		if ((od & 1) || (od > 1 && od < n)) cout << "YES\n";
-		else cout << "NO\n";
-	}
-	return 0;
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+return 0;
 }
