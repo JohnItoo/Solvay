d218b264e6b97c0d297852d2b7508b420010443d
JohnOhue <itoohue@gmail.com>
2020-08-01 23:00:01 +0100

    2020-08-01

--- codeforces/training/Zigzag.cpp
+++ codeforces/training/Zigzag.cpp
@@ -1,14 +1,40 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+
 int main() {
+
+  int n; 
+  cin  >> n;
+  int a[n];
+  for(int i = 0; i < n; i++) {
+  	 cin >> a[i];
+  }
+  vector<vector<int> > dp(n+1, vector<int>(2, 0));
+  dp[0][0] = 1;
+  dp[0][1] = 1;
+
+  int best = 1;
+  for(int i = 1; i < n; i++) {
+  	for(int j = 0; j < i ; j++) {
+  		if(a[j] < a[i]) dp[i][0] = max(dp[j][1] + 1, dp[i][0]);
+  		else if (a[j] > a[i]) dp[i][1] = max(dp[j][0] + 1, dp[i][0]);
+  	}
+  	best = max(dp[i][0], dp[i][1]);
+  }
+
+  cout << best << "\n";
+}
+
+//This solution is dp but it is crude.
+int unused() {
 	int n;
 	cin >> n;
 	int a[n + 1];
 	int prev[n + 1];
 	int dp[n + 1];
 	vector<int> vi;
-	map > int, int > mps;
+	map  <int, int > mps;
 
 
 	for (int i = 1; i <= n; i++) {
@@ -17,7 +43,7 @@ int main() {
 		else prev[i] = 1;
 		dp[i] = 2;
 		if (mps.find(a[i]) == mps.end()) {
-			mp[a[i]] = 1;
+			mps[a[i]] = 1;
 			vi.push_back(a[i]);
 		}
 	}
@@ -48,4 +74,5 @@ int main() {
 
 	cout << "\n";
 	cout << dp[n] << "\n";
+	return 0;
 }
