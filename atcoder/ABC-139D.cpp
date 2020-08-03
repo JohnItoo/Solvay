9bc844a315c8849753b200d3e8bdbb2e893058ef
JohnOhue <itoohue@gmail.com>
2020-08-04 00:00:00 +0100

    2020-08-04

---
+++ atcoder/ABC-139D.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	long long n;
+	cin >> n;
+	long long ans = ((n-1) * (n))/ 2;
+	cout << ans << "\n";
+}
--- atcoder/ABC138B.cpp
+++ atcoder/ABC138B.cpp
@@ -1,22 +1,15 @@
 #include <bits/stdc++.h>
 using namespace std;
-
 int main() {
 	int n; cin >> n;
 	vector<int> a(n);
 	for (int i = 0; i < n; i++) {
 		cin >> a[i];
 	}
-
-	sort(a.begin(), a.end());
-
 	double ans = 0;
-	double sm = a[0];
-	int i = 1;
-
-	while (i < n) {
-		sm = (a[i] + sm) / 2;
-		i++;
+	for (int i = 0; i < n; i++) {
+		ans += (double) 1 / a[i];
 	}
-	printf("%.2f\n", sm);
+
+	printf("%.9f\n",  1 / ans);
 }
