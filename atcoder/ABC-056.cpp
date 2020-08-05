f24b5d0229ab9d80b6a0132b717eb5316ce805ba
JohnOhue <itoohue@gmail.com>
2020-08-05 18:00:00 +0100

    2020-08-05

--- atcoder/ABC-056.cpp
+++ atcoder/ABC-056.cpp
@@ -4,16 +4,11 @@ using namespace std;
 int main() {
 	long long x;
 	cin >> x;
-	long long c = x * 2;
-	// cout << c << "\n";
-	long long div = 1 + (4 *c);
-	// cout << div << "\n";
-	long long root = pow(div, 0.5);
-	// cout << root << "\n";
-	if(root * root == div) {
-		long long ans = (root - 1) / 2;
-		cout << ans << "\n";
-	} else {
-		cout << x << "\n";
+	long long sum = 0;
+	long long i = 1;
+	while(sum < x) {
+       sum += i;
+       i++;
 	}
+	cout << i-1 << "\n";
 }
