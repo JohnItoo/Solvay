dc1ccc86463f2aab0df6bc68dfecaab70394dabb
JohnOhue <itoohue@gmail.com>
2020-08-05 23:00:00 +0100

    2020-08-05

---
+++ atcoder/ABC-137C.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+	int n; cin >> n;
+	vector<string> strings(n);
+	for (int i = 0; i < n; i++) {
+		string s; cin >> s;
+		sort(s.begin(), s.end());
+		strings[i] = s;
+	}
+	sort(strings.begin(), strings.end());
+	long long ans = 0;
+	long long curr = 0;
+	for (int i = 1; i < n; i++) {
+		if (strings[i] == strings[i - 1]) {
+			curr++;
+		} else {
+			long long dic = ((curr * (curr + 1)) / 2) - 1;
+			if (dic >= 0) ans += dic;
+			curr = 0;
+
+		}
+	}
+	long long dic = ((curr * (curr + 1)) / 2) - 1;
+	if (dic >= 0) ans += dic;
+
+	cout << ans << "\n";
+}
