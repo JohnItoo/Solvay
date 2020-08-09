7070ff155222ae2bad7248b2ef882839f4867b81
JohnOhue <itoohue@gmail.com>
2020-08-09 10:00:00 +0100

    2020-08-09

--- codeforces/CF144-D2-B.cpp
+++ codeforces/CF144-D2-B.cpp
@@ -1,6 +1,6 @@
 //============================================================================
 // Name        : template.cpp
-// Author      : 
+// Author      :
 // Version     :
 // Copyright   : Your copyright notice
 // Description : Hello World in C++, Ansi-style
@@ -40,7 +40,56 @@ for (msi::iterator it = (c).begin(); it != (c).end(); it++)
 //memset(arr, 0, sizeof arr); // useful to clear array of integers
 
 int main() {
-  ios::sync_with_stdio(false);
-  cin.tie(0);
-return 0;
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	int xa, ya, xb, yb; cin >> xa >> ya >> xb >> yb;
+	if (xa > xb) swap(xa, xb);
+
+	vector<ii> seats;
+
+	REP(x, xa, xb - 1) {
+		if (x != xa) {
+			seats.pb(mp(x, ya));
+			seats.pb(mp(x, yb));
+		} else {
+			int mny = min(ya, yb);
+			int mxy = max(ya, yb);
+			REP(y, mny, mxy) {
+				seats.pb(mp(x, y));
+				seats.pb(mp(xb, y));
+			}
+		}
+	}
+
+	int n; cin >> n;
+
+	vector<bool> htd(seats.size(), false);
+	forn(i, n) {
+		int x, y, r ; cin >> x  >> y >> r;
+		forn(j , seats.size()) {
+			if (htd[j]) continue;
+			ii seat = seats[j];
+			int x2 = seat.first;
+			int y2 = seat.second;
+
+			int xdiff = x - x2;
+			int ydiff = y - y2;
+			if ((xdiff * xdiff) + (ydiff * ydiff) <= (r * r)) {
+				htd[j] = true;
+			}
+		}
+	}
+
+	int ans = 0;
+
+	forn(i, htd.size()) {
+		if (!htd[i]) ans++;
+	}
+
+
+	cout << ans << "\n";
+
+
+
+	return 0;
 }
