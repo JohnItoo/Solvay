209b05f206b1ecdc94e89d577e4d40da93fdacc3
JohnOhue <itoohue@gmail.com>
2020-08-11 23:00:00 +0100

    2020-08-11

--- cses/ConcertTickets.cpp
+++ cses/ConcertTickets.cpp
@@ -4,44 +4,28 @@ using namespace std;
 int main() {
 	int n, m; cin >> n >> m;
 	multiset<int> tickets;
-	map<int, int> done;
-	int used = 0;
+
 	for (int i = 0; i < n; i++) {
 		int x; cin >> x;
 		tickets.insert(x);
-		done[x]++;
 	}
-	int last = 0;
+
 	for (int i = 0; i < m; i++) {
 		int p; cin >> p;
-
-		if (tickets.size() >= 1) {
+		if (tickets.size() > 0) {
 			auto it = tickets.lower_bound(p);
-			if (*it > p && it != tickets.begin()) --it;
+			if (it == tickets.end() || *it > p) --it;
 
 			int ans = -1;
-			// cout << *it << " it \n";
-			if (*it <= p) {
+			if (it != tickets.end() && *it <= p) {
 				ans = *it;
 				tickets.erase(it);
-				used++;
-				last = i;
 			}
-			// tickets.erase(it);
 
 			cout << ans << "\n";
-			if (used == n) break;
-
 		} else {
 			cout << -1 << "\n";
 		}
 
-
 	}
-
-	// for (int i = last + 1; i < m; i++) {
-	// 	cout << -1 << "\n";
-	// }
-
-
 }
