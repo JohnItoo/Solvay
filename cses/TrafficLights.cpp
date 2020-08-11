7734d4477fde195eaee3e34aa6ae03c1bf2a2b45
JohnOhue <itoohue@gmail.com>
2020-08-11 19:00:00 +0100

    2020-08-11

--- cses/TrafficLights.cpp
+++ cses/TrafficLights.cpp
@@ -3,29 +3,49 @@ using namespace std;
 typedef long long ll;
 
 int main() {
-	int x;
-	cin >> x;
-	int n; cin >> n;
-	set<int> s;
-	multiset<int> ms;
+	// int x;
+	// cin >> x;
+	// int n; cin >> n;
+	// set<int> s;
+	// multiset<int> ms;
+	// s.insert(0);
+	// s.insert(x);
+
+
+	// for (int i = 0; i < n; i++) {
+	// 	int pi; cin >> pi;
+	// 	auto l = s.lower_bound(pi), r = --s.upper_bound(pi);
+
+	// 	if (*l > pi) --l;
+	// 	if (*r < pi) ++r;
+
+	// 	if (ms.find(*r - *l) != ms.end()) ms.erase(ms.find(*r - *l));
+
+	// 	ms.insert(pi - *l);
+	// 	ms.insert(*r - pi);
+	// 	s.insert(pi);
+
+
+	// 	cout << *--ms.end() << " ";
+	// }
+
+	int x, n, a[200002];
+	set <int> s;
+	multiset <int> m;
+	cin >> x >> n;
 	s.insert(0);
-	s.insert(x); 
-
-
-	for (int i = 0; i < n; i++) {
-	   int pi; cin >> pi;
-		auto l = s.lower_bound(pi), r = --s.upper_bound(pi);
-		if(*l > pi) l--;
-		if(*r < pi) r++;
-
-		if(ms.find(*r - *l) != ms.end()) ms.erase(*r - *l);
-
-		ms.insert(*r-pi);
-		ms.insert(pi-*l);
-
-		auto end = ms.end()--;
-
-		cout << *end << " ";
-		s.insert(pi);
+	s.insert(x);
+	for (int i = 1; i <= n; ++i) {
+		cin >> a[i];
+		auto l = s.lower_bound(a[i]), r = --s.upper_bound(a[i]);
+		if (*l > a[i]) --l;
+		if (*r < a[i]) ++r;
+		auto f = m.find(*r - *l);
+		if (f != m.end()) m.erase(f);
+		m.insert(a[i] - *l);
+		m.insert(*r - a[i]);
+		s.insert(a[i]);
+		cout << *--m.end() << ' ';
 	}
+
 }
