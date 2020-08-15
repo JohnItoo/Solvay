b2ae62dd6e6c98054e8bba62d13cbeabaa36a291
JohnOhue <itoohue@gmail.com>
2020-08-15 14:00:00 +0100

    2020-08-15

--- atcoder/ABC-175.cpp
+++ atcoder/ABC-175.cpp
@@ -1,6 +1,6 @@
 //============================================================================
 // Name        : template.cpp
-// Author      : 
+// Author      :
 // Version     :
 // Copyright   : Your copyright notice
 // Description : Hello World in C++, Ansi-style
@@ -39,8 +39,106 @@ for (msi::iterator it = (c).begin(); it != (c).end(); it++)
 //memset(dp_memo, -1, sizeof dp_memo); // useful to initialize DP memoization table
 //memset(arr, 0, sizeof arr); // useful to clear array of integers
 
+// int main() {
+// 	ios::sync_with_stdio(false);
+//  cin.tie(0);
+//  string s; cin >> s;
+//  int rs = 0;
+//  int mx = 0;
+//  forn(i, s.length()) {
+//  	if(s[i] == 'R') {
+//  		rs++;
+//  		mx = max(rs, mx);
+//  	} else {
+//  		mx = max(rs, mx);
+//  		rs = 0;
+//  	}
+//  }
+//  cout  << mx << "\n";
+// return 0;
+// }
+
+
+
+// int main() {
+// 	ios::sync_with_stdio(false);
+// 	cin.tie(0);
+// 	int n = 0; cin >> n;
+// 	vector<ll> l(n + 1);
+// 	REP(i, 1, n) {
+// 		cin >> l[i];
+// 	}
+
+// 	map<string, int> mp;
+
+// 	int ans = 0;
+// 	for (int i = 1; i + 2 <= n; i++) {
+// 		for (int j = i + 1; j + 1 <= n; j++) {
+// 			for (int k = j + 1; k <= n; k++) {
+// 				if (l[i] != l[j] && l[j] != l[k]) {
+// 					// vector<ll> bs;
+// 					// bs.pb(l[i]); bs.pb(l[j]); bs.pb(l[k]);
+
+// 					// if (st.count(make_tuple(bs[0], bs[1], bs[2])) == 0) {
+// 					    string s;
+// 					    s.pb(l[i] -'0');
+// 					    s.pb(l[j] - '0');
+// 					    s.pb(l[k] - '0');
+// 					    sort(s.begin(), s.end());
+// 					    if(mp.find(s) == mp.end()) {
+// 					    	cout << l[i] << l[j] << l[k] << "here\n";
+// 						ans++;
+// 						mp[s] = 1;
+// 					    }
+
+// 					// }
+
+// 				}
+// 			}
+// 		}
+// 	}
+// 	cout << ans << "\n";
+// 	return 0;
+// }
+
+
 int main() {
 	ios::sync_with_stdio(false);
- cin.tie(0);
-return 0;
+	cin.tie(0);
+	ll x, k, d;
+	cin >> x >> k >> d;
+
+	if (x < 0) {
+
+		ll f =   0 - x;
+		if ( k * d < f) {
+			cout << abs(x + (k * d)) << "\n";
+		} else {
+			ll div = f / d;
+			ll rem = f - (div * d);
+			ll curr = x + (d * div);
+			k -= d;
+			if (k & 1) {
+				cout << min(abs(curr - d), abs(curr + d)) << "\n";
+			} else {
+				cout << curr << "\n";
+			}
+		}
+
+	} else {
+		ll f = x - 0; //steps to 0;
+		if (k * d < f) {
+			cout << f - (k * d) << "\n";
+		} else {
+			ll div =  x / d;
+			ll rem = x - (div * d);
+			k -= div;
+			if (k & 1) {
+				cout << min(rem + d, abs(rem - d)) << "\n";
+			} else {
+				cout << rem << "\n";
+			}
+		}
+	}
+	return 0;
 }
