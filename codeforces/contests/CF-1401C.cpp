b0de7a4dc365552d599449004a221d0ceb565a6d
JohnOhue <itoohue@gmail.com>
2020-08-21 16:00:01 +0100

    2020-08-21

--- codeforces/contests/CF-1401C.cpp
+++ codeforces/contests/CF-1401C.cpp
@@ -1,12 +1,18 @@
 //============================================================================
 // Name        : template.cpp
-// Author      :
+// Author      : 
 // Version     :
 // Copyright   : Your copyright notice
 // Description : Hello World in C++, Ansi-style
 //============================================================================
 
-#include <bits/stdc++.h>
+#include <iostream>
+#include <algorithm>
+#include <set>
+#include <map>
+#include <set>
+#include <vector>
+#include <string.h> // for memset in CF judge.
 using namespace std;
 #define _CRT_SECURE_NO_DEPRECATE // suppress some compilation warning messages (for VC++ users)
 // Shortcuts for "common" data types in contests
@@ -38,59 +44,9 @@ for (msi::iterator it = (c).begin(); it != (c).end(); it++)
 //memset(dist, MEMSET_INF, sizeof dist); // useful to initialize shortest path distances
 //memset(dp_memo, -1, sizeof dp_memo); // useful to initialize DP memoization table
 //memset(arr, 0, sizeof arr); // useful to clear array of integers
-// to_string(int)
-
-
-string trUpp(string s) {
-
-	transform(s.begin(), s.end(), s.begin(), ::toupper);
-	return "";
-}
-
-bool isPrime(int x) {
-	return (x == 2 || x == 3 || x == 5 || x == 7);
-}
 
 int main() {
-	int n; cin >> n;
-	string s; cin >> s;
-	sort(s.begin(), s.end());
-	set<int> brks;
-	map<int, int> mp;
-	mp.clear();
-	bool allPrime = true;
-	int arr[8][4] = { {1, 0, 0, 0}, {1, 1, 0, 0}, {3, 1, 0, 0}, {4, 2, 1, 0}, {5, 2, 1, 0}, {4, 2, 1, 1}, {7, 2, 1, 1}, {7, 4, 1, 1}};
-	int primes[4] = {2, 3, 5, 7};
-	for (int i = n - 1; i >= 0; i--) {
-		int curr = s[i] - '0';
-		if (curr < 2) continue;
-		if (isPrime(curr) && curr != 3) {
-			mp[curr]++;
-		} else {
-			forn(i, 4) {
-				if (arr[curr - 2][i] > 0) {
-					mp[primes[i]] += arr[curr - 2][i];
-
-				}
-			}
-		}
-
-	}
-
-	if (mp.find(3) != mp.end() && mp.find(2) != mp.end()) {
-		mp[2] -= mp.find(3)->second;
-	}
-
-	string ans = "";
-
-	for (int i = 3; i >= 0; i--) {
-		if (mp.find(primes[i]) != mp.end()) {
-			int times = mp.find(primes[i])->second;
-			char c = (char) primes[i] + '0';
-			string suff = string(times, c);
-			ans += suff;
-		}
-	}
-	cout << ans << "\n";
-	return 0;
+ ios::sync_with_stdio(false);
+ cin.tie(0);
+return 0;
 }
