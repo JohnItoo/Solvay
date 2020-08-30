d4a1f47ac7553ce62a8c32bec6e7085a0b453ecf
JohnOhue <itoohue@gmail.com>
2020-08-30 13:00:00 +0100

    2020-08-30

--- cses/Playlist.cpp
+++ cses/Playlist.cpp
@@ -8,17 +8,21 @@ int main() {
 
 	int ans = 0;
 	int curr = 0;
+	int last = 0;
 	for (int i = 0; i < n; i++) {
 		int k; cin >> k;
 		auto it = mp.find(k);
-		if (it == mp.end()) {
-			curr++;
-			mp[k] = i;
-		} else {
+		if (it != mp.end() && it->second >= last) {
 			curr = i - it->second;
+			last = it->second + 1;
 			if (curr == 1) mp.clear();
 			mp[k] = i;
+			// cout << curr <<  "reset\n";
 
+		} else {
+			curr++;
+			mp[k] = i;
+			// cout << curr << " inc\n";
 		}
 		ans = max(ans, curr);
 	}
