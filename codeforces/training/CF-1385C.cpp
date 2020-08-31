add9002d8b7b4dcbc3cb2e832c58bb4b894ed989
JohnOhue <itoohue@gmail.com>
2020-08-31 11:00:02 +0100

    2020-08-31

--- codeforces/training/CF-1385C.cpp
+++ codeforces/training/CF-1385C.cpp
@@ -1,6 +1,6 @@
 //============================================================================
 // Name        : template.cpp
-// Author      : 
+// Author      :
 // Version     :
 // Copyright   : Your copyright notice
 // Description : Hello World in C++, Ansi-style
@@ -40,7 +40,29 @@ for (msi::iterator it = (c).begin(); it != (c).end(); it++)
 //memset(arr, 0, sizeof arr); // useful to clear array of integers
 
 int main() {
-  ios::sync_with_stdio(false);
-  cin.tie(0);
-return 0;
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	int tc;
+	while (tc--) {
+		int n; cin >> n;
+		vector<int> a(n);
+		forn(i, n) cin >> a[i];
+
+		int prevMax ==
+		int currMax = 0;
+		int lastStart = 0;
+		int i = 0;
+		int j = n - 1;
+		int mn = 0;
+		while (i < j) {
+			if (a[i] < a[j]) {
+				currMax = max(a[i], currMax);
+				i++;
+			} else {
+				currMax = max(a[j], currMax);
+				j--;
+			}
+		}
+	}
+	return 0;
 }
