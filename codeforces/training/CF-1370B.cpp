c828de60ee17ce3592ea3e2b90236a5907e8eb18
JohnOhue <itoohue@gmail.com>
2020-09-06 00:00:00 +0100

    2020-09-06

--- codeforces/training/CF-1370B.cpp
+++ codeforces/training/CF-1370B.cpp
@@ -1,6 +1,6 @@
 //============================================================================
 // Name        : template.cpp
-// Author      : 
+// Author      :
 // Version     :
 // Copyright   : Your copyright notice
 // Description : Hello World in C++, Ansi-style
@@ -40,7 +40,75 @@ for (msi::iterator it = (c).begin(); it != (c).end(); it++)
 //memset(arr, 0, sizeof arr); // useful to clear array of integers
 
 int main() {
-  ios::sync_with_stdio(false);
-  cin.tie(0);
-return 0;
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	int tc;
+	cin >> tc;
+	while (tc--) {
+		int n; cin >> n;
+		set<ii> st;
+		vector<ii> odds;
+		vector<ii> evs;
+		vector<ii> result;
+		forn(i, n * 2) {
+			int a; cin >> a;
+			if (a & 1) odds.pb(make_pair(a, i));
+			else evs.pb(make_pair(a, i));
+		}
+		if (odds.size() < evs.size()) {
+			int q = 0;
+			while (q + 1 < odds.size() - 2) {
+				ii pr = mp(odds[q].second + 1, odds[q + 1].second + 1);
+				result.pb(pr);
+				q += 2;
+			}
+			if (q < odds.size() - 2) {
+				st.insert(odds[q]);
+			}
+			forn(i, evs.size()) {
+				st.insert(evs[i]);
+				if (st.size() == (2 * n) - 2) break;
+			}
+		} else {
+			int q = 0;
+			int ct = 0;
+			while (q + 1 < evs.size() - 2) {
+				ii pr = mp(evs[q].second + 1, evs[q + 1].second + 1);
+				result.pb(pr);
+				q += 2;
+				ct += 2;
+			}
+			if (q < evs.size() - 2) {
+				st.insert(evs[q]);
+				ct += 1;
+			}
+			cout << ct << "\n";
+
+			forn(i, odds.size()) {
+				st.insert(odds[i]);
+				if (st.size() == ((2 * n) - 2) ) break;
+			}
+		}
+
+		while (st.size() != 0) {
+			auto it = st.begin();
+			ii fr = *it;
+			auto iit = st.erase(it);
+			ii sec;
+			if (iit != st.end()) {
+				sec =  *iit;
+			}
+
+			st.erase(iit);
+
+			result.pb(fr);
+			result.pb(sec);
+		}
+
+		forn(i, result.size()) {
+			cout << result[i].first << " " << result[i].second << "\n";
+		}
+
+	}
+	return 0;
 }
