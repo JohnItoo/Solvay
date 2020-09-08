8223b8a29996d0bf1b3625339f04ddbc33907373
JohnOhue <itoohue@gmail.com>
2020-09-08 20:00:00 +0100

    2020-09-08

--- codeforces/contests/CF-1407C.cpp
+++ codeforces/contests/CF-1407C.cpp
@@ -1,6 +1,6 @@
 //============================================================================
 // Name        : template.cpp
-// Author      : 
+// Author      :
 // Version     :
 // Copyright   : Your copyright notice
 // Description : Hello World in C++, Ansi-style
@@ -46,7 +46,56 @@ for (msi::iterator it = (c).begin(); it != (c).end(); it++)
 //memset(arr, 0, sizeof arr); // useful to clear array of integers
 
 int main() {
- ios::sync_with_stdio(false);
- cin.tie(0);
-return 0;
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	int tc; cin >> tc;
+	while (tc--) {
+		int n; cin >> n;
+		vi a(n + 1);
+		int odds = 0;
+		int evens = 0;
+		REP(i, 1, n) {
+			int c; cin >> c;
+			a[i] = c;
+			if (i & 1) odds += c;
+			else evens += c;
+		}
+		bool done = false;
+		multiset<int> skip;
+		if (odds == evens) {
+			cout << n << "\n";
+			REP(i, 1, n) {
+				cout << a[i] << " ";
+			}
+			cout << "\n";
+			done = true;
+		} else {
+			int diff = abs(evens - odds);
+			bool evs = evens > odds;
+			int i = 1;
+			while (diff > 0 && i <= n) {
+				if (a[i] == 0) {
+					i++;
+					continue;
+				}
+				if (evs && (i % 2 == 0)) {
+					skip.insert(i);
+					diff--;
+				}
+				if (!evs && i & 1) {
+					skip.insert(i);
+					diff--;
+				}
+				i++;
+			}
+			cout << n - skip.size() << "\n";
+			REP(i, 1, n) {
+				if (skip.lower_bound(i) == skip.end()) {
+					cout << a[i] << " ";
+				}
+			}
+			cout << "\n";
+		}
+	}
+	return 0;
 }
