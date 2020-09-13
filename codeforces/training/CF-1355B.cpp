b8a9f2d19c9ad4c0cc7d92784858e4a053b4ef59
JohnOhue <itoohue@gmail.com>
2020-09-13 22:00:00 +0100

    2020-09-13

---
+++ codeforces/training/CF-1355B.cpp
@@ -0,0 +1,68 @@
+//============================================================================
+// Name        : template.cpp
+// Author      :
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <bits/stdc++.h>
+using namespace std;
+#define _CRT_SECURE_NO_DEPRECATE // suppress some compilation warning messages (for VC++ users)
+// Shortcuts for "common" data types in contests
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int, int> ii;
+typedef vector<ii> vii;
+typedef set<int> si;
+typedef map<string, int> msi;
+// To simplify repetitions/loops, Note: define your loop style and stick with it!
+#define x first
+#define y second
+#define pb push_back
+#define mp make_pair
+#define REP(i, a, b) \
+for (int i = int(a); i <= int(b); i++) // a to b, and variable i is local!
+#define forn(i,n) \
+for (int i =0; i<(n); i++)
+#define TRvi(c, it) \
+for (vi::iterator it = (c).begin(); it != (c).end(); it++)
+#define TRvii(c, it) \
+for (vii::iterator it = (c).begin(); it != (c).end(); it++)
+#define TRmsi(c, it) \
+for (msi::iterator it = (c).begin(); it != (c).end(); it++)
+#define INF 2000000000 // 2 billion
+// If you need to recall how to use memset:
+#define MEMSET_INF 127 // about 2B
+#define MEMSET_HALF_INF 63 // about 1B
+//memset(dist, MEMSET_INF, sizeof dist); // useful to initialize shortest path distances
+//memset(dp_memo, -1, sizeof dp_memo); // useful to initialize DP memoization table
+//memset(arr, 0, sizeof arr); // useful to clear array of integers
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	int tc; cin >> tc;
+	while (tc--) {
+		int n; cin >> n;
+		set<int> st;
+		map<int, int> mp;
+		forn(i, n) {
+			int x; cin >> x;
+			mp[x]++;
+			st.insert(x);
+		}
+
+		int rem = 0;
+		int ans = 0;
+		for (auto nt : st) {
+			int count = mp[nt];
+			int use = ((rem + count) / nt) * nt;
+			if (!use) break;
+			rem = (rem + count) - use;
+			ans += use;
+		}
+		cout << ans << "\n";
+	}
+	return 0;
+}
