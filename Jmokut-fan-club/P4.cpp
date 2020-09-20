e5f84e615431eec67c95d3ad6f9c4ffdc6458251
JohnOhue <itoohue@gmail.com>
2020-09-20 19:00:00 +0100

    2020-09-20

--- Jmokut-fan-club/P4.cpp
+++ Jmokut-fan-club/P4.cpp
@@ -1,18 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
-
+//https://acm.timus.ru/problem.aspx?space=1&num=1073
 
 int main() {
 	int mx = 1e9 + 8;
 	vector<int> dp(60500, mx);
 	dp[0] = 0;
-	// dp[1] = 1;
 	int n; cin >> n;
 
 	for (int i = 1; i <= n; i++) {
 		for (int j = 1; j * j <= i; j++) {
 
-			dp[i] = min(dp[i],  dp[i - j] + 1);
+			dp[i] = min(dp[i],  dp[i - (j * j)] + 1);
 		}
 	}
 	cout << dp[n] << "\n";
