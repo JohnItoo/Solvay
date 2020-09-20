0e01de0e0babb72b460c1ae0eb21806c17d26fdf
JohnOhue <itoohue@gmail.com>
2020-09-21 00:00:00 +0100

    2020-09-21

--- Jmokut-fan-club/P5.cpp
+++ Jmokut-fan-club/P5.cpp
@@ -1,21 +1,28 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+#define ll long long
+
 int main() {
     int n; cin >> n;
-    vector<vector<int> > dp(n + 5, vector<int>(n + 5, 0));
+    vector<vector<ll> > dp(n + 5, vector<ll>(n + 5, 0));
 
     dp[0][0] = 1;
+
+    for (int i = 1; i <= n; i++) {
+        dp[0][i] = dp[1][i] = 1;
+    }
+
     for (int i = 2; i <= n; i++) { //bricks
-        for (int j = 1; j < i; j++) {
+        for (int j = 1; j < n; j++) { // last height
             dp[i][j] = dp[i][j - 1];
 
             if (i >= j) {
-
+                dp[i][j] += dp[i - j][j - 1];
             }
         }
     }
-    cout << dp[n][n] << "\n";
+    cout << dp[n][n - 1] << "\n";
 
     return 0;
 }
