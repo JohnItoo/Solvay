eddfbde34eab7be9d514dfdf8824f88a5aa645de
JohnOhue <itoohue@gmail.com>
2020-09-20 23:00:00 +0100

    2020-09-20

--- Jmokut-fan-club/P5.cpp
+++ Jmokut-fan-club/P5.cpp
@@ -2,19 +2,20 @@
 using namespace std;
 
 int main() {
-	int n; cin >> n;
-	vector<vector<int> > dp(n + 5, vector<int>(n + 5, 0));
+    int n; cin >> n;
+    vector<vector<int> > dp(n + 5, vector<int>(n + 5, 0));
 
+    dp[0][0] = 1;
+    for (int i = 2; i <= n; i++) { //bricks
+        for (int j = 1; j < i; j++) {
+            dp[i][j] = dp[i][j - 1];
 
-	for (int steps = 2; steps < n; steps++) { // with at most this steps
-		dp[i][j] = dp[i][j - 1];
-		for (int k = 2; k < i; k++) {
-			dp[i][j] += dp[i - k][j - 1] + 1;
-		}
+            if (i >= j) {
 
-	}
+            }
+        }
+    }
+    cout << dp[n][n] << "\n";
 
-	cout << dp[n][n] << "\n";
-
-	return 0;
+    return 0;
 }
