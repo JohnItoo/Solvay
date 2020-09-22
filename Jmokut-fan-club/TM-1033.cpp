7b2cfda64f9f6fdd21ffc66c3abd8df7160abf83
JohnOhue <itoohue@gmail.com>
2020-09-22 23:00:00 +0100

    2020-09-22

---
+++ Jmokut-fan-club/TM-1033.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+	int n; cin >> n;
+	vector<vector<char > > grid(n, vector<char>(n));
+
+	for (int i = 0; i < n; i++) {
+		string x;
+		cin >> x;
+		for (int j = 0; j < n; j++) {
+			grid[i][j] = string[j];
+		}
+	}
+
+	return 0;
+}
