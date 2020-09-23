8bb3d439e555d25f690a9d2a11c5ccf245be8340
JohnOhue <itoohue@gmail.com>
2020-09-23 17:00:00 +0100

    2020-09-23

--- Timus-OJ/TM-1087.cpp
+++ Timus-OJ/TM-1087.cpp
@@ -3,20 +3,17 @@ using namespace std;
 
 int main () {
 	int n, m; cin >> n >> m;
+	vector<int> dp(10005, 0);
+
 	vector<int> k(m);
-	int odds = 0; int evens = 0;
 	for (int i = 0; i < m; i++) {
 		cin >> k[i];
-		if (k[i] & 1) odds++;
-		else evens++;
 	}
 
-	if (n & 1 && evens == 0) {
-		cout << 1 << "\n";
-	} else if ( n % 2 == 0 && odds >= 1) {
-		cout << 1 << "\n";
-	} else {
-		cout << 2 << "\n";
+	sort(k.begin(), k.end());
+	int lst = k[0];
+	for (int i = 1; i < n; i++) {
+		lst |= k[i];
 	}
 	return 0;
 }
