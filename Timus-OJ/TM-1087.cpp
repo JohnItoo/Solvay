73bfd3f282501784062319a6767855b84b987572
JohnOhue <itoohue@gmail.com>
2020-09-23 20:00:01 +0100

    2020-09-23

--- Timus-OJ/TM-1087.cpp
+++ Timus-OJ/TM-1087.cpp
@@ -8,12 +8,19 @@ int main () {
 	vector<int> k(m);
 	for (int i = 0; i < m; i++) {
 		cin >> k[i];
+		// dp[k[i]] = 0;
 	}
+	dp[0] = 1;
 
-	sort(k.begin(), k.end());
-	int lst = k[0];
 	for (int i = 1; i < n; i++) {
-		lst |= k[i];
+		for (int j = 0; j < m; j++) {
+			if (i + k[j] > n) continue;
+			if (dp[i] == 0) {
+				dp[i + k[j]] = 1;
+			}
+		}
 	}
+	if (dp[n] == 1) cout << 1 << endl;
+	else cout << 2 << endl;
 	return 0;
 }
