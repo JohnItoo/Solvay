3a660c42e35ac3a0df36d2947c50223fd2fd1bf7
JohnOhue <itoohue@gmail.com>
2020-09-24 07:00:00 +0100

    2020-09-24

--- Timus-OJ/TM-1032.cpp
+++ Timus-OJ/TM-1032.cpp
@@ -5,31 +5,34 @@ using namespace std;
 
 int main () {
 	int n; cin >> n;
-	// vector<ll> pref(n + 1);
-	ll pref[n + 1];
-	ll a[n + 1];
-	// vector<ll> a(n + 1);
+	vector<ll> pref(n + 1, 0);
+	vector<ll> a(n + 1, 0);
+
+	int left = 0;
+	int right = 0;
 	for (int i = 1; i <= n; i++) {
 		cin >> a[i];
 		pref[i] = a[i] + pref[i - 1];
-
+		if (pref[i] % n == 0)  {
+			right = i;
+		}
 	}
-	bool ans = false;
-	int left = 0; int right = 0;
-	for (int i = 1; i <= n; i++) {
-		for (int j = 0; j <= n; j++) {
-			if (i == j) continue;
-			if (abs(pref[i] - pref[j]) % n == 0 ) {
-				ans = true;
-				left = i;
-				right = j;
-				break;
+	map<int, int> mp;
 
+	if (right == 0) {
+		for (int i = 1; i <= n; i++) {
+			int mod = pref[i] % n;
+			if (mp.find(mod) != mp.end()) {
+				right = i;
+				left = mp[mod];
+				break;
+			} else {
+				mp[mod] = i;
 			}
 		}
-		if (ans) break;
+
 	}
-	if (left > right) swap(left, right);
+
 	cout << right - left << "\n";
 	for (int i = left + 1; i <= right; i++) {
 		cout << a[i] << "\n";
