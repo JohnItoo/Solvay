4adf873d516df3e6aca595fbed72a3e0bd827f2a
JohnOhue <itoohue@gmail.com>
2020-10-01 12:00:00 +0100

    2020-10-01

--- codeforces/contests/CF-1408C.cpp
+++ codeforces/contests/CF-1408C.cpp
@@ -1,6 +1,6 @@
 //============================================================================
 // Name        : template.cpp
-// Author      : 
+// Author      :
 // Version     :
 // Copyright   : Your copyright notice
 // Description : Hello World in C++, Ansi-style
@@ -46,7 +46,60 @@ for (msi::iterator it = (c).begin(); it != (c).end(); it++)
 //memset(arr, 0, sizeof arr); // useful to clear array of integers
 
 int main() {
- ios::sync_with_stdio(false);
- cin.tie(0);
-return 0;
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	int tc; cin >> tc;
+	while (tc--) {
+		int n; double l; cin >> n >> l;
+		vector<double> flags(n);
+
+		forn(i, n) cin >> flags[i];
+
+		int i = 0;
+		int j = n - 1;
+		double presa = 0;
+		double presb = l;
+		double speeda = 1;
+		double speedb = 1;
+		double totaltime = 0;
+		while (i < j) {
+
+			double dista = flags[i] - presa;
+			double timea = dista / speeda;
+
+			double distb = presb - flags[j];
+			double timeb = distb / speeda;
+
+			printf("% .2f  %.2f\n" , timea, timeb);
+
+			if (timea == timeb) {
+				speeda += 1;
+				speedb += 1;
+				presa = flags[i++];
+				presb = flags[j--];
+
+				totaltime += timea;
+			} else {
+				if (timea < timeb) {
+					presa = flags[i++];
+					presb = timea * speedb;
+					speeda += 1;
+					totaltime += timea;
+				} else {
+					presb = flags[j--];
+					presa = timeb * speeda;
+					speedb += 1;
+					totaltime += timeb;
+				}
+			}
+
+		}
+		double meet = ((speeda * presb) + (speedb * presa)) / (speeda + speedb);
+		double rem = meet - presa;
+		double tmmeet =  rem / speeda;
+
+		printf("% .7f \n" , tmmeet + totaltime);
+
+	}
+	return 0;
 }
