6c46dff67158966c00064fd63b12b7a17d921db0
JohnOhue <itoohue@gmail.com>
2020-10-02 01:00:00 +0100

    2020-10-02

--- Timus-OJ/TM-1045.cpp
+++ Timus-OJ/TM-1045.cpp
@@ -3,17 +3,29 @@ using namespace std;
 
 vector<int> g[1005];
 pair<int, int> mp;
+vector<bool> visited(1005, false);
 
 void dfs(int k, int parent,  int depth) {
-	if (g[k].size() == 1) {
+	if (visited[k]) return;
+
+	visited[k] = true;
+
+	int ct = g[k].size();
+	for (int e : g[k]) {
+		if (visited[e]) ct--;
+	}
+	if (ct == 0) {
 		if ((depth & 1) && parent < mp.second) {
 			mp = make_pair(depth, parent);
 		}
-		return;
-	}
-	for (int e : g[k]) {
-		int p = depth == 0 ? e : parent;
-		dfs(e, p, depth + 1);
+
+	} else {
+		for (int e : g[k]) {
+			if (!visited[e]) {
+				int p = depth == 0 ? e : parent;
+				dfs(e, p, depth + 1);
+			}
+		}
 	}
 }
 
@@ -21,7 +33,7 @@ void dfs(int k, int parent,  int depth) {
 int main() {
 	mp = make_pair(10005, 10005);
 	int n, k; cin >> n >> k;
-	for (int i = 0; i < n; i++) {
+	for (int i = 0; i < n - 1; i++) {
 		int x, y; cin >> x >> y;
 		g[x].push_back(y);
 		g[y].push_back(x);
@@ -30,8 +42,9 @@ int main() {
 	if (mp.first == 10005) {
 		cout << "First player loses\n";
 	} else {
-		printf("First player wins flying to airport %d", mp.second);
+		printf("First player wins flying to airport %d\n", mp.second);
 	}
 
 	return 0;
 }
+
