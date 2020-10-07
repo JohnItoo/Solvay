bbe8a763937bd3dbecfb2373f620bf0d7e722753
JohnOhue <itoohue@gmail.com>
2020-10-08 00:00:00 +0100

    2020-10-08

--- atcoder/ARC-60C.cpp
+++ atcoder/ARC-60C.cpp
@@ -13,10 +13,10 @@ int main() {
 		sm += x[i];
 	}
 
-	vector<vector<ll> > dp(sm + 1, vector<ll>(n + 1, 0));
+	vector<vector<ll> > dp(2550, vector<ll>(n + 1, 0));
 	dp[0][0] = 1;
 
-	for (int i = 1; i <= sm; i++) {
+	for (int i = 2500; i >= 1; i--) {
 		for (int j = 1; j <= n; j++) {
 			for (int k = 0; k < n; k++) {
 				int card = x[k];
@@ -26,6 +26,16 @@ int main() {
 
 		}
 	}
+	// for (int i = 0; i < n; i++) {
+	// 	for (int j = 2500; j >= 1; j--) {
+	// 		for (int k = 1; k <= n; k++) {
+	// 			int card = x[i];
+	// 			if (card > j) continue;
+	// 			dp[j][k] += dp[j - card][k - 1];
+	// 		}
+
+	// 	}
+	// }
 
 	ll val = 1;
 
@@ -35,10 +45,10 @@ int main() {
 	while (val <= n && a * val <= sm) {
 		ll curr = a * val;
 		ll ct = dp[curr][val];
-		cout << ct << " " << val << "\n";
-		if (val != 1) {
-			ct /= 2;
-		}
+		// cout << ct << " " << val << "\n";
+		// if (val != 1) {
+		// 	ct /= 2;
+		// }
 		val++;
 		ans += ct;
 	}
