69e675078b92f4991bc399adcd1f0b8005a566e6
JohnOhue <itoohue@gmail.com>
2020-10-12 14:00:00 +0100

    2020-10-12

---
+++ codeforces/training/CF-591B.cpp
@@ -0,0 +1,79 @@
+//============================================================================
+// Name        : template.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <bits/stdc++.h>
+using namespace std;
+#define _CRT_SECURE_NO_DEPRECATE // suppress some compilation warning messages (for VC++ users)
+// Shortcuts for "common" data types in contests
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int, int> ii;
+typedef vector<ii> vii;
+typedef set<int> si;
+typedef map<string, int> msi;
+// To simplify repetitions/loops, Note: define your loop style and stick with it!
+#define x first
+#define y second
+#define pb push_back
+#define mp make_pair
+#define REP(i, a, b) \
+for (int i = int(a); i <= int(b); i++) // a to b, and variable i is local!
+#define forn(i,n) \
+for (int i =0; i<(n); i++)
+#define TRvi(c, it) \
+for (vi::iterator it = (c).begin(); it != (c).end(); it++)
+#define TRvii(c, it) \
+for (vii::iterator it = (c).begin(); it != (c).end(); it++)
+#define TRmsi(c, it) \
+for (msi::iterator it = (c).begin(); it != (c).end(); it++)
+#define INF 2000000000 // 2 billion
+// If you need to recall how to use memset:
+#define MEMSET_INF 127 // about 2B
+#define MEMSET_HALF_INF 63 // about 1B
+//memset(dist, MEMSET_INF, sizeof dist); // useful to initialize shortest path distances
+//memset(dp_memo, -1, sizeof dp_memo); // useful to initialize DP memoization table
+//memset(arr, 0, sizeof arr); // useful to clear array of integers
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+  vector<int> arr[26];
+  int n,m; cin >> n >> m;
+  string s; cin >> s;
+  forn(i, s.length()) {
+  	arr[s[i] - 'a'].pb(i);
+  }
+  forn(i, m) {
+  	char x,y; cin >> x >> y;
+  	vi sss = arr[x-'a'];
+  	vi qqq = arr[y-'a'];
+  	vi nw;
+  	forn(j, sss.size()) {
+  		nw.pb(sss[j]);
+  	}
+  	vi mw;
+  	forn(j, qqq.size()) {
+  		mw.pb(qqq[j]);
+  	}
+  	arr[y-'a'] = nw;
+  	arr[x-'a'] = mw;
+  	// erase(arr[x-'a'].begin(), arr[x-'a'].end());
+  	
+  }
+
+  string res = string(s.length(), '.');
+
+  forn(i, 26) {
+  	vi curr = arr[i];
+  	for(auto el : curr) {
+  		res[el] = i + 'a';
+  	}
+  }
+  cout << res << "\n";
+return 0;
+}
