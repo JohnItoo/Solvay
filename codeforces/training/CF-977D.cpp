d2fd5323cd0029f8862d48290cf3dfec00949126
JohnOhue <itoohue@gmail.com>
2020-10-20 16:00:00 +0100

    2020-10-20

---
+++ codeforces/training/CF-977D.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+	int n; cin >> n;
+	map<int, int> vals;
+	for(int i = 0; i < n; i++) {
+		int x;
+		cin >> x;
+		vals[x]++;
+	}
+
+	for(int i = 0; i < n; i++) {
+		int ct = 1;
+		map<int, int> used;
+		
+		while(ct < n) {
+
+
+		}
+	}
+
+
+	return 0;
+}
