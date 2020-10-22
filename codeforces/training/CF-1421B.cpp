a31d1cb4286f62dfa87d6d4f3237c9ed1eb88ebb
JohnOhue <itoohue@gmail.com>
2020-10-22 02:00:00 +0100

    2020-10-22

--- codeforces/training/CF-1421B.cpp
+++ codeforces/training/CF-1421B.cpp
@@ -38,56 +38,33 @@ for (msi::iterator it = (c).begin(); it != (c).end(); it++)
 //memset(dist, MEMSET_INF, sizeof dist); // useful to initialize shortest path distances
 //memset(dp_memo, -1, sizeof dp_memo); // useful to initialize DP memoization table
 //memset(arr, 0, sizeof arr); // useful to clear array of integers
-int dir[6][2] = { { -1, 0}, {0, -1}, {1, 0}, {-1, -1}, {1,1}, {0, 1}};
+int dir[4][2] = { { -1, 0}, {0, -1}, {1, 0}, {0, 1}};
 map<ii, int> visited;
-int res[2][2];
+bool fd = false;
 
-
-void dfs(string arr[], bool one, ii pt, int n) {
+void dfs(string arr[], char val, ii pt, int n) {
 
 	if (visited.find(pt) != visited.end()) return;
 	visited[pt] = 1;
-	if ((pt.first == n - 2 && pt.second == n - 1) || (pt.first == n - 1 && pt.second == n - 2)) {
-		if (one) {
-			res[1][0] = pt.first + 1;
-			res[1][1] = pt.second + 1;
-		} else {
-			res[0][0] = pt.first + 1;
-			res[0][0] = pt.second + 1;
+	if ((pt.first == 0 && pt.second == 1) || (pt.first == 1 && pt.second == 0)) {
+	    if(arr[pt.first][pt.second] == val) {
+	    	fd = true;
 
-		}
+	    }
 		return;
 	}
 
-	for (int i = 0; i < 6; i++) {
+	for (int i = 0; i < 4; i++) {
 		int nr = pt.first + dir[i][0];
 		int nc = pt.second + dir[i][1];
 		if (nr < 0 || nr >= n || nc < 0 || nc >= n) {
 			continue;
 		}
-		if ((one && arr[nr][nc] == '0') || (!one && arr[nr][nc] == '1')) {
+		if (arr[nr][nc] != val) {
 			continue;
 		}
-		dfs(arr, one, mp(nr, nc), n);
+		dfs(arr, val, mp(nr, nc), n);
 	}
-}
-
-vector<vii> call(string arr[], int n) {
-
-	vector<vii> ret;
-
-	for (int i = 0; i < 2; i++) {
-		memset(res, -1, sizeof res);
-		visited.clear();
-		dfs(arr, i, mp(0, 0), n);
-		vii a;
-		a.pb(mp(res[0][0], res[0][1]));
-		a.pb(mp(res[1][0], res[1][1]));
-		ret.pb(a);
-	}
-
-	return ret;
-
 }
 
 
@@ -101,39 +78,25 @@ int main() {
 		forn(i, n) {
 			cin >> arr[i];
 		}
-		vector<vii> dz = call(arr, n);
-		bool zero = dz[0][0].first != -1 || dz[0][1].first != -1;
-		bool one = dz[1][0].first != -1 || dz[1][1].first != -1;
-		
-		if(!zero && !one) {
-			cout << 0 << "\n";
-		} else {
-			if(one && zero) {
-				cout << 2 << "\n";
-				cout << n<< " " << n-1 << "\n";
-				cout << n-1 << " "<<  n << endl;
-			} else {
-				vii ff;
-				if(one && (arr[n-1][n-2] == '1')) {
-					ff.pb(mp(n-1, n-2));
-				}
-				if(one && (arr[n-2][n-1] == '1')) {
-					ff.pb(mp(n-2, n-1));
-				}
-
-				if(zero && (arr[n-1][n-2] == '0')) {
-					ff.pb(mp(n, n-1));
-				}
-				if(zero && (arr[n-2][n-1] == '0')) {
-					ff.pb(mp(n-1, n));
-				}
-				cout << ff.size() << "\n";
-				for(int k = 0; k < ff.size(); k++) {
-					cout << ff[k].first << " " << ff[k].second << "\n";
-				}
-
-			}
+		fd = false;
+		dfs(arr, arr[n-2][n-1], mp(n-2, n-1), n);
+		vii a;
+		if(fd) {
+			fd = false;
+			a.pb(mp(n-1, n));
 		}
+
+		dfs(arr, arr[n-1][n-2], mp(n-1, n-2), n);
+
+		if(fd) {
+			a.pb(mp(n, n-1));
+		}
+		
+		cout << a.size() << "\n";
+		for(ii el : a) {
+			cout << el.first << " " << el.second << "\n";
+		}
+		
 	}
 	return 0;
 }
