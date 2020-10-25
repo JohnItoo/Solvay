79139365a364507158308df53a0052243e195dae
JohnOhue <itoohue@gmail.com>
2020-10-25 14:00:00 +0100

    2020-10-25

--- usaco/Triangles.cpp
+++ usaco/Triangles.cpp
@@ -15,24 +15,8 @@ int main() {
 
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < n; j++) {
-			if (i == j) continue;
-			if (vertices[i].first == vertices[j].first || vertices[i].second == vertices[j].second) {
-
-				for (int k = 0; k < n; k++) {
-					if (k == i || k == j) continue;
-
-					pair<int, int> first = vertices[i];
-					pair<int, int> second = vertices[j];
-					pair<int, int> third = vertices[k];
-					// xequal base
-
-					if (first.first == second.first) {
-
-					}
-
-
-
-				}
+			for (int k = 0; k < n; k++) {
+				if (i == k || i == j || k == j) continue;
 
 			}
 		}
