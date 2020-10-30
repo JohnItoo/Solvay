cfa8843579aae0194069fb5c47ee6dbaef173e86
JohnOhue <itoohue@gmail.com>
2020-10-30 17:00:00 +0100

    2020-10-30

--- cses/Labyrinth.cpp
+++ cses/Labyrinth.cpp
@@ -2,45 +2,62 @@
 using namespace std;
 vector<string> room(1500);
 vector<vector<bool > > visited(1500, vector<bool>(1500, false));
-vector<vector<int> > distance(1500, vector<int>(1500, 0));
+vector<vector<int> > dist(1500, vector<int>(1500, -1));
 int n = 0 , m = 0;
+
 string ans = "";
 map<char, pair<int, int> > mv;
 
 
 void walk(int row, int col) {
 
+
 	queue<pair<pair<int, int>, int> > q;
 	q.push(make_pair(make_pair(row, col), 0));
 	visited[row][col] = true;
-	distance[row][col] = 0;
+	dist[row][col] = 0;
 
 	while (!q.empty()) {
 		pair<pair<int, int>, int> tp = q.front(); q.pop();
 		int row = tp.first.first;
 		int col = tp.first.second;
+		int d = tp.second;
+		dist[row][col] = d;
 
 
 		if (room[row][col] == 'B') {
-			//backtrack
+			string res;
+			//backtrack and build
+
 			while (room[row][col] != 'A') {
-				if (row > 0 && distance[row - 1][col] == distance[row][col] - 1) {
-
+				if (row > 0 && dist[row - 1][col] == d - 1) {
+					res.push_back('D');
+					row--;
+				} else if (row < n - 1 && dist[row + 1][col] == d - 1) {
+					res.push_back('U');
+					row++;
+				} else if (col > 0 && dist[row][col - 1] == d - 1) {
+					res.push_back('R');
+					col--;
+				} else  {
+					res.push_back('L');
+					col++;
 				}
+				d--;
 			}
-
-
+			reverse(res.begin(), res.end());
+			ans = res;
 			break;
 		} else {
 			for (auto ii : mv) {
+
 				int nr = mv[ii.first].first + row;
 				int nc = mv[ii.first].second + col;
-				if (nr < 0 || nr >= n || nc < 0 || nc >= m) continue;
+				if (nr < 0 || nr >= n || nc < 0 || nc >= m || room[nr][nc] == '#') continue;
 				if (visited[nr][nc]) continue;
 
 				visited[nr][nc] = true;
-				pair<pair<int, int> > np = make_pair(make_pair(nr, nc), tp.second + 1);
-				distance[nr][nc] = np.second;
+				pair<pair<int, int>, int > np = make_pair(make_pair(nr, nc), d + 1);
 				q.push(np);
 			}
 		}
@@ -64,9 +81,10 @@ int main() {
 				r = i;
 				c = j;
 			}
+
 		}
 	}
-	walk(r, c, "");
+	walk(r, c);
 	if (ans == "") {
 		cout << "NO\n";
 	} else {
