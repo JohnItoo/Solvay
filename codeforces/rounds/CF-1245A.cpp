1f7c604616e89f58a9defd6e425ae9bbc7954d0f
JohnOhue <itoohue@gmail.com>
2020-11-04 10:00:00 +0100

    2020-11-04

--- codeforces/rounds/CF-1245A.cpp
+++ codeforces/rounds/CF-1245A.cpp
@@ -1,6 +1,6 @@
 //============================================================================
 // Name        : template.cpp
-// Author      : 
+// Author      :
 // Version     :
 // Copyright   : Your copyright notice
 // Description : Hello World in C++, Ansi-style
@@ -45,8 +45,22 @@ for (msi::iterator it = (c).begin(); it != (c).end(); it++)
 //memset(dp_memo, -1, sizeof dp_memo); // useful to initialize DP memoization table
 //memset(arr, 0, sizeof arr); // useful to clear array of integers
 
+int gcd(int a, int b) {
+	if (b == 0) return a;
+	return gcd(b , a % b);
+}
+
 int main() {
- ios::sync_with_stdio(false);
- cin.tie(0);
-return 0;
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	int tc; cin >> tc;
+	while (tc--) {
+		int n, m; cin >> n >> m;
+		if (gcd(n, m) != 1) {
+			cout << "Infinite\n";
+		} else {
+			cout << "Finite\n";
+		}
+	}
+	return 0;
 }
--- codeforces/rounds/CF-1245B.cpp
+++ codeforces/rounds/CF-1245B.cpp
@@ -1,6 +1,6 @@
 //============================================================================
 // Name        : template.cpp
-// Author      : 
+// Author      :
 // Version     :
 // Copyright   : Your copyright notice
 // Description : Hello World in C++, Ansi-style
@@ -46,7 +46,50 @@ for (msi::iterator it = (c).begin(); it != (c).end(); it++)
 //memset(arr, 0, sizeof arr); // useful to clear array of integers
 
 int main() {
- ios::sync_with_stdio(false);
- cin.tie(0);
-return 0;
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	int tc; cin >> tc;
+	while (tc--) {
+		int n; cin >> n;
+		int a, b, c; cin >> a >> b >> c;
+		string s; cin >> s;
+		int x = 0, y = 0, z = 0;
+		forn(i, s.length()) {
+			if (s[i] == 'R') y++;
+			else if (s[i] == 'P') z++;
+			else x++;
+		}
+		int tot = min(a, x) + min(b, y) + min(c, z);
+		if (tot < (n + 1) / 2) {
+			cout << "NO\n";
+		} else {
+			cout << "YES\n";
+			string ans;
+			forn(i, n) {
+				if (s[i] == 'S' && a > 0) {
+					ans.pb('R');
+					a--;
+				} else if (s[i] == 'R' && b > 0) {
+					ans.pb('P');
+					b--;
+				} else if (s[i] == 'P' && c > 0) {
+					ans.pb('S');
+					c--;
+				} else {
+					if (a > 0) {
+						ans.pb('R');
+						a--;
+					} else if (b > 0) {
+						ans.pb('P');
+						b--;
+					} else {
+						ans.pb('S');
+						c--;
+					}
+				}
+			}
+			cout << ans << "\n";
+		}
+	}
+	return 0;
 }
