44c8c64d3ce03ec2c3d4e8da17d56a0cee5b20b1
JohnOhue <itoohue@gmail.com>
2020-11-09 14:00:00 +0100

    2020-11-09

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Solvay
 
-**This is a peek into some of my solutions to random questions from different CP platforms like the UVa Online Judge, CF etcetera.
-At the moment, code is not pretty as most of the files were scripted in pseudo-contest mode, subsequent pushes to the repo
-should adress this and provide links to the questions**
+This is a collection of my solution to Algorithm and DataStructure problems from different platforms.
+This is a simple reference and library. All directories are named after some coding platform and the solutions are descriptively named.
+Find a link to my certificates of participation and ranks [here](https://johnohue.me/work).
--- cses/TwoKnights.cpp
+++ cses/TwoKnights.cpp
@@ -4,13 +4,19 @@ using namespace std;
 int main() {
 	int g; cin >> g;
 
+	//Total Number of ways to place 2 things on n*n cells is  = n^2(n^2 + 1) / 2;
+	//A knight will attack in a 2*3 or 3*2 square.  Number of 2*3 squares + number of 3* 2 squares ==
+	// (n-2)(n-1) + (n-1)(n-2) = 2(n-1)(n-2)
+	// but each of these above ways has 2 ways of placing 2 knights = 4(n-1)(n-2)
+
+	// answer is n^2(n^2 + 1) / 2 - 4(n-1)(n-2)
 	for (int n = 1; n <= g; n++) {
 		long long sq = (n * n);
 		long long totalPairs = sq * (sq - 1);
 		long long totalWaysToPlaceTwo = totalPairs / 2;
 
-		long long waysToPlaceNoAttack = 4 * ( (n - 1) * (n - 2));
-		long long ans = totalWaysToPlaceTwo - waysToPlaceNoAttack;
+		long long waysToPlaceAttacks = 4 * ( (n - 1) * (n - 2));
+		long long ans = totalWaysToPlaceTwo - waysToPlaceAttacks;
 		cout <<  ans << "\n";
 	}
 
