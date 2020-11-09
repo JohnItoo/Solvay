c137b43e76017fd71b3b03f345d0503a491aaebd
JohnOhue <itoohue@gmail.com>
2020-11-09 23:00:00 +0100

    2020-11-09

--- usaco/AngryCows.cpp
+++ usaco/AngryCows.cpp
@@ -1,11 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int recur(int radius, int start,  map<int, int> &mp) {
+int recur(int radius, int start,  map<int, int> &mp, int n) {
 	int ct = 0;
+	if(start >= n) return 0;
 	for (int i = start; i <= radius + start; i++) {
 		if (mp.find(i) != mp.end())ct++;
 	}
+
+	return ct + recur(radius+1, radius+start, mp);
+}
+
+int precur(int radius, int start, map<int, int> &mp) {
+	int ct = 0;
+	if(start)
+	for(int i = start; i >= radius - start; i--) {
+		if(mp.find(i) != mp.end()) ct++;
+	}
+	return ct + precur(radius+1, radius - start, mp);
 }
 
 int main() {
