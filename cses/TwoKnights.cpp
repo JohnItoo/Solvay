560cba4ed7c8bbf8c38714c1f7cbc46e23da3739
JohnOhue <itoohue@gmail.com>
2020-11-09 11:00:00 +0100

    2020-11-09

--- cses/TwoKnights.cpp
+++ cses/TwoKnights.cpp
@@ -5,14 +5,14 @@ int main() {
 	int g; cin >> g;
 
 	for (int n = 1; n <= g; n++) {
-		long long totalPairs = (n * n) * ((n * n) - 1);
+		long long sq = (n * n);
+		long long totalPairs = sq * (sq - 1);
 		long long totalWaysToPlaceTwo = totalPairs / 2;
 
 		long long waysToPlaceNoAttack = 4 * ( (n - 1) * (n - 2));
 		long long ans = totalWaysToPlaceTwo - waysToPlaceNoAttack;
-		cout << ans << "\n";
+		cout <<  ans << "\n";
 	}
-
 
 	return 0;
 }
