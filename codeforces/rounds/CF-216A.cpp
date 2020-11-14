273a6dc8b8374ab129db2d52d2c07e24cadac72b
JohnOhue <itoohue@gmail.com>
2020-11-14 18:00:00 +0100

    2020-11-14

--- codeforces/rounds/CF-216A.cpp
+++ codeforces/rounds/CF-216A.cpp
@@ -1,9 +1,57 @@
-#include <bits/stdc++.h>
+//============================================================================
+// Name        : template.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+#include <algorithm>
+#include <set>
+#include <map>
+#include <set>
+#include <vector>
+#include <string.h> // for memset in CF judge.
 using namespace std;
+#define _CRT_SECURE_NO_DEPRECATE // suppress some compilation warning messages (for VC++ users)
+// Shortcuts for "common" data types in contests
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int, int> ii;
+typedef vector<ii> vii;
+typedef set<int> si;
+typedef map<string, int> msi;
+// To simplify repetitions/loops, Note: define your loop style and stick with it!
+#define x first
+#define y second
+#define pb push_back
+#define mp make_pair
+#define REP(i, a, b) \
+for (int i = int(a); i <= int(b); i++) // a to b, and variable i is local!
+#define forn(i,n) \
+for (int i =0; i<(n); i++)
+#define TRvi(c, it) \
+for (vi::iterator it = (c).begin(); it != (c).end(); it++)
+#define TRvii(c, it) \
+for (vii::iterator it = (c).begin(); it != (c).end(); it++)
+#define TRmsi(c, it) \
+for (msi::iterator it = (c).begin(); it != (c).end(); it++)
+#define INF 2000000000 // 2 billion
+// If you need to recall how to use memset:
+#define MEMSET_INF 127 // about 2B
+#define MEMSET_HALF_INF 63 // about 1B
+//memset(dist, MEMSET_INF, sizeof dist); // useful to initialize shortest path distances
+//memset(dp_memo, -1, sizeof dp_memo); // useful to initialize DP memoization table
+//memset(arr, 0, sizeof arr); // useful to clear array of integers
 
 int main() {
-	long long a, b, c; cin >> a >> b >> c;
-	long long ans = (2 * b) + ((2 * c) - 2);
-	ans += (b - 1) * (c - 1);
-	cout << ans << "\n";
+ ios::sync_with_stdio(false);
+ cin.tie(0);
+ int a, b, c; cin >> a >> b >> c;
+ int per = a+b + c -3;
+ int ans = per * 2;
+ int ins = (b-1) * (c-1);
+ cout << ans + ins << endl;
+return 0;
 }
