54c8c8aa7c276816becaa017d145fa7128066b7a
JohnOhue <itoohue@gmail.com>
2020-11-15 22:00:00 +0100

    2020-11-15

--- codeforces/training/CF-1169A.cpp
+++ codeforces/training/CF-1169A.cpp
@@ -4,16 +4,21 @@ using namespace std;
 int main() {
 	int n, a , x , b , y; cin >> n >> a >> x >> b  >> y;
 
-	int time = abs(a - b);
-	if (time % 2 == 0) {
-		int lef = x < a ? (n - x ) + (x - a) : x - a;
-		int right = y > b ? (b - 1) + (n - y) : n - y;
-		if (lef >= time && right >= time) {
-			cout << "YES\n";
+	while(a != x && b != y && a != b) {
+		if(a == n) {
+			a = 1;
 		} else {
-			cout << "NO\n";
+			a++;
 		}
 
+		if(y == 1) {
+			y = n;
+		} else {
+			y--;
+		}
+	}
+	if(a == b) {
+		cout << "YES\n";
 	} else {
 		cout << "NO\n";
 	}
