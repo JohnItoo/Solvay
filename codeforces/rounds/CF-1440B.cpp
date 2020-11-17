798824c01c384728d7243b5f4f9b5916054722fc
JohnOhue <itoohue@gmail.com>
2020-11-17 20:00:00 +0100

    2020-11-17

---
+++ codeforces/rounds/CF-1440B.cpp
@@ -0,0 +1,76 @@
+//============================================================================
+// Name        : template.cpp
+// Author      :
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <bits/stdc++.h>
+using namespace std;
+#define _CRT_SECURE_NO_DEPRECATE // suppress some compilation warning messages (for VC++ users)
+// Shortcuts for "common" data types in contests
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int, int> ii;
+typedef vector<ii> vii;
+typedef set<int> si;
+typedef map<string, int> msi;
+// To simplify repetitions/loops, Note: define your loop style and stick with it!
+#define x first
+#define y second
+#define pb push_back
+#define mp make_pair
+#define REP(i, a, b) \
+for (int i = int(a); i <= int(b); i++) // a to b, and variable i is local!
+#define forn(i,n) \
+for (int i =0; i<(n); i++)
+#define TRvi(c, it) \
+for (vi::iterator it = (c).begin(); it != (c).end(); it++)
+#define TRvii(c, it) \
+for (vii::iterator it = (c).begin(); it != (c).end(); it++)
+#define TRmsi(c, it) \
+for (msi::iterator it = (c).begin(); it != (c).end(); it++)
+#define INF 2000000000 // 2 billion
+// If you need to recall how to use memset:
+#define MEMSET_INF 127 // about 2B
+#define MEMSET_HALF_INF 63 // about 1B
+//memset(dist, MEMSET_INF, sizeof dist); // useful to initialize shortest path distances
+//memset(dp_memo, -1, sizeof dp_memo); // useful to initialize DP memoization table
+//memset(arr, 0, sizeof arr); // useful to clear array of integers
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	int tc; cin >> tc;
+	while (tc--) {
+		int n, k;
+		cin >> n >> k;
+		vi a(n * k);
+		forn(i, n * k) {
+			cin >> a[i];
+		}
+		int lm = n * k;
+		sort(a.begin(), a.end());
+
+		int picked = 0;
+		int i = lm - 1;
+		int md = (n + 1) / 2;
+		ll ans = 0;
+		while (picked < k) {
+			int ct = 0;
+			while (ct < n - md) {
+				i--;
+				ct++;
+			}
+			ans = (ll) a[i] + ans;
+			picked++;
+			i--;
+		}
+		cout << ans << endl;
+
+	}
+
+
+	return 0;
+}
