4113129e5815841ab621e0eaacfac439a997fb51
JohnOhue <itoohue@gmail.com>
2020-11-20 18:00:00 +0100

    2020-11-20

--- codeforces/rounds/CF-955C.cpp
+++ codeforces/rounds/CF-955C.cpp
@@ -1,18 +1,12 @@
 //============================================================================
 // Name        : template.cpp
-// Author      : 
+// Author      :
 // Version     :
 // Copyright   : Your copyright notice
 // Description : Hello World in C++, Ansi-style
 //============================================================================
 
-#include <iostream>
-#include <algorithm>
-#include <set>
-#include <map>
-#include <set>
-#include <vector>
-#include <string.h> // for memset in CF judge.
+#include<bits/stdc++.h>
 using namespace std;
 #define _CRT_SECURE_NO_DEPRECATE // suppress some compilation warning messages (for VC++ users)
 // Shortcuts for "common" data types in contests
@@ -44,9 +38,36 @@ for (msi::iterator it = (c).begin(); it != (c).end(); it++)
 //memset(dist, MEMSET_INF, sizeof dist); // useful to initialize shortest path distances
 //memset(dp_memo, -1, sizeof dp_memo); // useful to initialize DP memoization table
 //memset(arr, 0, sizeof arr); // useful to clear array of integers
+int H, D, C, N;
+double calculate(int mins, bool disc) {
+	int curr = H + (D * mins);
+	int BB = (curr + N - 1) / N;
+	if (disc) {
+		return  (double) (C * BB) - (0.2 * C * BB);
+	} else {
+		return (double) C * BB;
+	}
+}
 
 int main() {
- ios::sync_with_stdio(false);
- cin.tie(0);
-return 0;
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	int hh , mm; cin >> hh >> mm;
+	cin >> H >> D >> C >> N;
+	if (hh < 20) {
+		int mins = ((20 - hh) * 60) - mm;
+		double mn = 1e9+4;
+		forn(i, mins) {
+			mn = min(mn, calculate(i, false));
+
+		}
+				double first = calculate(mins, true);
+		// cout << setprecision(15) << min(first, second) << endl;
+		printf("%.3f\n", min(first, mn));
+	} else {
+		// cout << setprecision(15) << calculate(0, false) << endl;
+		printf("%.3f\n", calculate(0, true));
+
+	}
+	return 0;
 }
