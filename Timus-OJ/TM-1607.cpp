6156730bab22cf69718857d71c40a995d16df677
JohnOhue <itoohue@gmail.com>
2020-12-10 11:00:00 +0100

    2020-12-10

--- Timus-OJ/TM-1607.cpp
+++ Timus-OJ/TM-1607.cpp
@@ -3,8 +3,8 @@ using namespace std;
 int main() {
 	long long a, b, c, d ; cin >> a >> b >> c >> d;
 	long long x = ((c - a) + (b + d - 1)) / (b + d);
-	cout << a + (b * x) << endl;
-
+	x -= 1; 
+	cout << max((a + b * x) + b, (c - d*x) -d) << endl;
 
 	return 0;
 }
