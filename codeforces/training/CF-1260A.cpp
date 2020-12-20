d455077314a19168dca40f94e1fc476347de8c44
JohnOhue <itoohue@gmail.com>
2020-12-20 15:00:00 +0100

    2020-12-20

--- codeforces/training/CF-1260A.cpp
+++ codeforces/training/CF-1260A.cpp
@@ -32,10 +32,20 @@ for (msi::iterator it = (c).begin(); it != (c).end(); it++)
 //memset(arr, 0, sizeof arr); // useful to clear array of integers
 
 int main() {
-  ios::sync_with_stdio(false);
-  cin.tie(0);
-  double h,l; cin >> h >> l;
-  double ans = ((l*l) - (h*h)) / (2 * h);
-  printf("%.14f\n", ans);
-return 0;
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	int tc; cin >> tc;
+	while (tc--) {
+		ll c, sum; cin >> c >>  sum;
+		if (sum <= c) {
+			cout << sum << endl;
+			continue;
+		}
+
+		ll k = sum / c;
+		ll ans = (k * k * (c - sum % c)) + ((k + 1) * (k + 1) * (sum % c));
+		cout <<  ans << endl;
+
+	}
+	return 0;
 }
