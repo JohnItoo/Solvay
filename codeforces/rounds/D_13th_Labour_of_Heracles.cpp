5e635a05709c691a5b75eaac5b7f8d8f26785486
JohnOhue <itoohue@gmail.com>
2021-01-23 13:00:00 +0100

    2021-01-23

---
+++ codeforces/rounds/D_13th_Labour_of_Heracles.cpp
@@ -0,0 +1,92 @@
+//============================================================================
+// Name        : template.cpp
+// Author      :
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <bits/stdc++.h>
+using namespace std;
+#define _CRT_SECURE_NO_DEPRECATE  // suppress some compilation warning messages
+                                  // (for VC++ users)
+// Shortcuts for "common" data types in contests
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int, int> ii;
+typedef vector<ii> vii;
+typedef set<int> si;
+typedef map<string, int> msi;
+// To simplify repetitions/loops, Note: define your loop style and stick with
+// it!
+#define x first
+#define y second
+#define pb push_back
+#define mp make_pair
+#define REP(i, a, b) \
+    for (int i = int(a); i <= int(b); i++)  // a to b, and variable i is local!
+#define forn(i, n) for (int i = 0; i < (n); i++)
+#define TRvi(c, it) for (vi::iterator it = (c).begin(); it != (c).end(); it++)
+#define TRvii(c, it) for (vii::iterator it = (c).begin(); it != (c).end(); it++)
+#define TRmsi(c, it) for (msi::iterator it = (c).begin(); it != (c).end(); it++)
+#define INF 2000000000  // 2 billion
+// If you need to recall how to use memset:
+#define MEMSET_INF 127      // about 2B
+#define MEMSET_HALF_INF 63  // about 1B
+// memset(dist, MEMSET_INF, sizeof dist); // useful to initialize shortest path
+// distances memset(dp_memo, -1, sizeof dp_memo); // useful to initialize DP
+// memoization table memset(arr, 0, sizeof arr); // useful to clear array of
+// integers
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int tc;
+    cin >> tc;
+    while (tc--) {
+        int n;
+        cin >> n;
+        vi a(n);
+        vi pref(n);
+        forn(i, n) {
+            cin >> a[i];
+            if (i == 0)
+                pref[i] = a[i];
+            else
+                pref[i] = pref[i - 1] + a[i];
+        }
+        vi en(n - 1);
+        forn(i, n - 1) {
+            int x, y;
+            cin >> x >> y;
+            en[i] = a[x - 1] + a[y - 1];
+        }
+        sort(en.rbegin(), en.rend());
+
+        // cout << pref[n-1] << " ";
+        forn(i, n - 1) cout << en[i] << endl;
+        return 0;
+        int i = 0;
+        int ct = 1;
+        while (ct < n - 2) {
+            int j = i;
+            int curr = pref[n - 1 - ct];
+            // cout << curr << "here\n";
+            int time = 0;
+            // cout << i << " ";
+            while (time < ct) {
+                curr += en[i++];
+                time++;
+                // cout << curr << " ";
+            }
+            // cout << " brr\n";
+            cout << curr << " ";
+            ct++;
+        }
+        int sm = 0;
+        forn(i, n - 1) sm += en[i];
+        cout << sm << endl;
+    }
+
+    return 0;
+}
