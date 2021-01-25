fe65034f625a9b6b458573e540a1c0803d8d30ad
JohnOhue <itoohue@gmail.com>
2021-01-25 19:00:00 +0100

    2021-01-25

--- atcoder/DP-F.cpp
+++ atcoder/DP-F.cpp
@@ -2,21 +2,32 @@
 using namespace std;
 
 int main() {
-    string s, t; cin >> s >> t;
+    string s, t;
+    cin >> s >> t;
     int n = s.length();
     int m = t.length();
-    vector<vector<int> > dp(n+5, vector<int>(m+5, 0));
+    vector<vector<int> > dp(n + 5, vector<int>(m + 5, 0));
 
-    for(int i = 1; i <= n; i++) {
-        for(int j = 1; j <= m; j++) {
-            if(s[i-1] == t[j-1]){
-                dp[i][j] = 1 + dp[i-1][j-1];
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= m; j++) {
+            if (s[i - 1] == t[j - 1]) {
+                dp[i][j] = 1 + dp[i - 1][j - 1];
             } else {
-                dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
             }
         }
     }
-    cout << dp[n][m] << endl;
+    int last = 0;
+    string ans = "";
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= m; j++) {
+            if (dp[i][j] > last) {
+                last = dp[i][j];
+                ans.push_back(s[i - 1]);
+            }
+        }
+    }
+    cout << ans << endl;
 
     return 0;
 }
