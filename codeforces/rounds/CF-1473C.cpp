46dd640dd3eb762d0d21d302b85b6b6ce50c99ed
JohnOhue <itoohue@gmail.com>
2021-02-11 19:00:00 +0100

    2021-02-11

---
+++ codeforces/rounds/CF-1473C.cpp
@@ -0,0 +1,85 @@
+// Problem: C. No More Inversions
+// Contest: Codeforces - Educational Codeforces Round 102 (Rated for Div. 2)
+// URL: https://codeforces.com/contest/1473/problem/C
+// Memory Limit: 256 MB
+// Time Limit: 2000 ms
+// 
+// Powered by CP Editor (https://cpeditor.org)
+
+// Problem: A. Replacing Elements
+// Contest: Codeforces - Educational Codeforces Round 102 (Rated for Div. 2)
+// URL: https://codeforces.com/contest/1473/problem/A
+// Memory Limit: 256 MB
+// Time Limit: 2000 ms
+// 
+// Powered by CP Editor (https://cpeditor.org)
+
+// Problem: B. Kolya and Tanya
+// Contest: Codeforces - Codeforces Round #324 (Div. 2)
+// URL: https://codeforces.com/contest/584/problem/B
+// Memory Limit: 256 MB
+// Time Limit: 1000 ms
+// 
+// Powered by CP Editor (https://cpeditor.org)
+//============================================================================
+// Name        : template.cpp
+// Author      :
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+ 
+#include <bits/stdc++.h>
+using namespace std;
+#define _CRT_SECURE_NO_DEPRECATE  // suppress some compilation warning messages
+                                  // (for VC++ users)
+// Shortcuts for "common" data types in contests
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int, int> ii;
+typedef vector<ii> vii;
+typedef set<int> si;
+typedef map<string, int> msi;
+// To simplify repetitions/loops, Note: define your loop style and stick with
+// it!
+#define x first
+#define y second
+#define pb push_back
+#define mp make_pair
+#define REP(i, a, b) \
+    for (int i = int(a); i <= int(b); i++)  // a to b, and variable i is local!
+#define forn(i, n) for (int i = 0; i < (n); i++)
+#define TRvi(c, it) for (vi::iterator it = (c).begin(); it != (c).end(); it++)
+#define TRvii(c, it) for (vii::iterator it = (c).begin(); it != (c).end(); it++)
+#define TRmsi(c, it) for (msi::iterator it = (c).begin(); it != (c).end(); it++)
+#define INF 2000000000  // 2 billion
+// If you need to recall how to use memset:
+#define MEMSET_INF 127      // about 2B
+#define MEMSET_HALF_INF 63  // about 1B
+// memset(dist, MEMSET_INF, sizeof dist); // useful to initialize shortest path
+// distances memset(dp_memo, -1, sizeof dp_memo); // useful to initialize DP
+// memoization table memset(arr, 0, sizeof arr); // useful to clear array of
+// integers
+// to_string(int)
+ 
+string trUpp(string s) {
+    transform(s.begin(), s.end(), s.begin(), ::toupper);
+    return "";
+}
+ 
+int main() {
+ int tc; cin >> tc;
+ while(tc--) {
+ 	int n,k; cin >> n >> k;
+ 	if(n == 1 && k == 1) {
+ 		cout << 1 << endl;
+ 	} else if( n != k) {
+ 		REP(i, 1, k-2) cout << i << " ";
+ 		cout << k << " " << k-1 << endl;
+ 	} else {
+ 		REP(i, 1, k) cout << i <<" ";
+ 		cout << endl;
+ 	}
+ 	
+ }
+}
