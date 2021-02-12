9a1dbd3e220949ff1da138359e4e455223ea4302
JohnOhue <itoohue@gmail.com>
2021-02-12 03:06:06 +0100

    2021-02-12

---
+++ codeforces/rounds/CF-479B.cpp
@@ -0,0 +1,72 @@
+// Problem: B. Random Teams
+// Contest: Codeforces - Codeforces Round #273 (Div. 2)
+// URL: https://codeforces.com/problemset/problem/478/B
+// Memory Limit: 256 MB
+// Time Limit: 1000 ms
+// 
+// Powered by CP Editor (https://cpeditor.org)
+
+//============================================================================
+// Name        : template.cpp
+// Author      :   $%U%$   
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <bits/stdc++.h>
+using namespace std;
+#define _CRT_SECURE_NO_DEPRECATE  // suppress some compilation warning messages
+                                  // (for VC++ users)
+// Shortcuts for "common" data types in contests
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int, int> ii;
+typedef vector<ii> vii;
+typedef set<int> si;
+typedef map<string, int> msi;
+// To simplify repetitions/loops, Note: define your loop style and stick with
+// it!
+#define x first
+#define y second
+#define pb push_back
+#define mp make_pair
+#define REP(i, a, b) \
+    for (int i = int(a); i <= int(b); i++)  // a to b, and variable i is local!
+#define forn(i, n) for (int i = 0; i < (n); i++)
+#define TRvi(c, it) for (vi::iterator it = (c).begin(); it != (c).end(); it++)
+#define TRvii(c, it) for (vii::iterator it = (c).begin(); it != (c).end(); it++)
+#define TRmsi(c, it) for (msi::iterator it = (c).begin(); it != (c).end(); it++)
+#define INF 2000000000  // 2 billion
+// If you need to recall how to use memset:
+#define MEMSET_INF 127      // about 2B
+#define MEMSET_HALF_INF 63  // about 1B
+// memset(dist, MEMSET_INF, sizeof dist); // useful to initialize shortest path
+// distances memset(dp_memo, -1, sizeof dp_memo); // useful to initialize DP
+// memoization table memset(arr, 0, sizeof arr); // useful to clear array of
+// integers
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    
+    
+    ll n, m; cin >> n >> m;
+    ll mdmx = n -m;
+    ll kmx = (mdmx* (mdmx+1))/2;
+    
+   
+    ll r2 = (n + m-1) /m; // full
+    ll r1fq = n%m;
+    ll r2fq = m - r1fq;
+    ll r1 = n/m;
+    ll lef =( r1 * (r1-1)) / 2;
+    ll rt = (r2 * (r2-1))/2;
+    lef *= r1fq;
+    rt *= r2fq;
+    
+ 
+    
+    ll kmn =lef + rt;
+    cout << kmn << " " << kmx << "\n";
+}
