2d4b6695d9c51f6ec81446bda886d308407eb6e5
JohnOhue <itoohue@gmail.com>
2021-03-02 10:00:00 +0100

    2021-03-02

--- codeforces/rounds/CF-1462D.cpp
+++ codeforces/rounds/CF-1462D.cpp
@@ -1,20 +1,23 @@
+// Problem: D. Add to Neighbour and Remove
+// Contest: Codeforces - Codeforces Round #690 (Div. 3)
+// URL: https://codeforces.com/problemset/problem/1462/D
+// Memory Limit: 256 MB
+// Time Limit: 3000 ms
+// 
+// Powered by CP Editor (https://cpeditor.org)
+
 //============================================================================
 // Name        : template.cpp
-// Author      :
+// Author      :   $%U%$   
 // Version     :
 // Copyright   : Your copyright notice
 // Description : Hello World in C++, Ansi-style
 //============================================================================
 
-#include <iostream>
-#include <algorithm>
-#include <set>
-#include <map>
-#include <set>
-#include <vector>
-#include <string.h> // for memset in CF judge.
+#include <bits/stdc++.h>
 using namespace std;
-#define _CRT_SECURE_NO_DEPRECATE // suppress some compilation warning messages (for VC++ users)
+#define _CRT_SECURE_NO_DEPRECATE  // suppress some compilation warning messages
+                                  // (for VC++ users)
 // Shortcuts for "common" data types in contests
 typedef long long ll;
 typedef vector<int> vi;
@@ -22,61 +25,77 @@ typedef pair<int, int> ii;
 typedef vector<ii> vii;
 typedef set<int> si;
 typedef map<string, int> msi;
-// To simplify repetitions/loops, Note: define your loop style and stick with it!
+// To simplify repetitions/loops, Note: define your loop style and stick with
+// it!
 #define x first
 #define y second
 #define pb push_back
 #define mp make_pair
 #define REP(i, a, b) \
-for (int i = int(a); i <= int(b); i++) // a to b, and variable i is local!
-#define forn(i,n) \
-for (int i =0; i<(n); i++)
-#define TRvi(c, it) \
-for (vi::iterator it = (c).begin(); it != (c).end(); it++)
-#define TRvii(c, it) \
-for (vii::iterator it = (c).begin(); it != (c).end(); it++)
-#define TRmsi(c, it) \
-for (msi::iterator it = (c).begin(); it != (c).end(); it++)
-#define INF 2000000000 // 2 billion
+    for (int i = int(a); i <= int(b); i++)  // a to b, and variable i is local!
+#define forn(i, n) for (int i = 0; i < (n); i++)
+#define TRvi(c, it) for (vi::iterator it = (c).begin(); it != (c).end(); it++)
+#define TRvii(c, it) for (vii::iterator it = (c).begin(); it != (c).end(); it++)
+#define TRmsi(c, it) for (msi::iterator it = (c).begin(); it != (c).end(); it++)
+#define INF 2000000000  // 2 billion
 // If you need to recall how to use memset:
-#define MEMSET_INF 127 // about 2B
-#define MEMSET_HALF_INF 63 // about 1B
-//memset(dist, MEMSET_INF, sizeof dist); // useful to initialize shortest path distances
-//memset(dp_memo, -1, sizeof dp_memo); // useful to initialize DP memoization table
-//memset(arr, 0, sizeof arr); // useful to clear array of integers
+#define MEMSET_INF 127      // about 2B
+#define MEMSET_HALF_INF 63  // about 1B
+// memset(dist, MEMSET_INF, sizeof dist); // useful to initialize shortest path
+// distances memset(dp_memo, -1, sizeof dp_memo); // useful to initialize DP
+// memoization table memset(arr, 0, sizeof arr); // useful to clear array of
+// integers
 
 int main() {
-	ios::sync_with_stdio(false);
-	cin.tie(0);
-	int tc; cin >> tc;
-	while (tc--) {
-		int n; cin >> n;
-		vector<ll> pref(n + 1);
-		set<ll> num;
-		REP(i, 1, n) {
-			ll x; cin >> x;
-			pref[i] += x + pref[i - 1];
-			num.insert(x);
-		}
-		if (num.size() == 1) {
-			cout << 0 << endl;
-			continue;
-		}
-		int ans = n - 1;
-		for (int i = 1; i <= n; i++) {
-			for (int j = i; j <= n; j++) {
-				int x = pref[i - 1];
-				int y = i == j ? pref[i] : pref[j] - pref[i];
-				int z = pref[n] - pref[j];
-				if (x == 0 && z == 0) continue;
-				if (x == 0) {
-					if (y % (j - i) == 0 &&  y / (j - i) == z) {
-						ans = n - j - 1
-					}
-				}
-			}
-		}
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int tc; cin >> tc;
+    while(tc--) {
+    	int n; cin >> n;
+    	vector<ll> a(n);
+    	forn(i, n) cin >> a[i];
+    	vector<ll> pref(n);
+    	pref[0] = a[0];
 
-	}
-	return 0;
+    	map<ll, int> mp;
+    	mp[a[0]] = 1;
+    	REP(i, 1, n-1) {
+    		 pref[i] = pref[i-1] + a[i];
+    		 mp[pref[i]] = i+ 1;
+    		 
+    	}
+    	int j = n-1;
+    	int rt = n-2;
+    	int i = rt;
+    	int ans = n-1;
+    	while(rt >= 0) {
+    		ll diff = pref[j] - pref[i];
+    		//cout << diff << "\n";
+    		int mv = 0;
+
+    		bool fd = false;
+    		while(i >= 0) {
+    			if(pref[j] - pref[i] > diff) break;
+    			if(pref[j] - pref[i] != diff) {
+    				i--;
+    				continue;
+    			}
+    			mv += j - i - 1;
+    			if(pref[i] == diff) {
+    				fd = true;
+    				mv += i;
+    				break;
+    			}
+    			j = i;
+    			i--;
+    		}
+    		if(fd && pref[j] - pref[i] == diff) ans = min(ans, mv);
+    		j = n-1;
+    		rt--;
+    		i = rt;
+    	}
+    	cout << ans << endl;
+
+    }
+    return 0;
 }
