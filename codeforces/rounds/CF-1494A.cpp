8c31d8620828f722aed8a471d7e87e54bf2fa92b
JohnOhue <itoohue@gmail.com>
2021-03-04 23:00:00 +0100

    2021-03-04

--- codeforces/rounds/CF-1494A.cpp
+++ codeforces/rounds/CF-1494A.cpp
@@ -3,10 +3,11 @@
 // URL: https://codeforces.com/contest/1494/problem/A
 // Memory Limit: 256 MB
 // Time Limit: 2000 ms
-// 
-// Powered by CP Editor (https://cpeditor.org)//============================================================================
+//
+// Powered by CP Editor
+// (https://cpeditor.org)//============================================================================
 // Name        : template.cpp
-// Author      :   $%U%$   
+// Author      :   $%U%$
 // Version     :
 // Copyright   : Your copyright notice
 // Description : Hello World in C++, Ansi-style
@@ -30,7 +31,7 @@ typedef map<string, int> msi;
 #define pb push_back
 #define mp make_pair
 #define REP(i, a, b) \
-    for (int i = int(a); i <= int(b); i++)  // a to b, and variable i is local!
+  for (int i = int(a); i <= int(b); i++)  // a to b, and variable i is local!
 #define forn(i, n) for (int i = 0; i < (n); i++)
 #define TRvi(c, it) for (vi::iterator it = (c).begin(); it != (c).end(); it++)
 #define TRvii(c, it) for (vii::iterator it = (c).begin(); it != (c).end(); it++)
@@ -45,64 +46,73 @@ typedef map<string, int> msi;
 // integers
 
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(0);
-    int tc; cin >> tc;
-    while(tc--) {
-    	string s;
-    	cin >> s;
-    	int n = s.length();
-    	if(s[0] == s[n-1]) {
-    		cout << "NO\n";
-    	} else {
-  
-    		int cl = 0;
-    		int i = 0;
-    		int seen = -1;
-    		bool corr = true;
-    		string one = "";
-    		forn(i, n) {
-    			if(s[i] == s[0]) one.pb('(');
-    			else if(s[i] == s[n-1]) one.pb(')');
-    			else one.pb('(');
-    		}
-    		
-    		string two = "";
-    		forn(i, n) {
-    			if(s[i] == s[0]) two.pb('(');
-    			else if(s[i] == s[n-1]) two.pb(')');
-    			else two.pb(')');
-    		}
-    		
-    		int op = 0; 
-    		bool fd = true;
-    		forn(i, one.size()) {
-    			if(one[i] == '(') op++;
-    			else op--;
-    			if(op < 0)   {
-    				fd = false;
-    				break;
-    			}
-    		}
-    		fd = fd && (op == 0);
-    		int ox = 0;
-    		bool gd = true;
-    		forn(i, one.size()) {
-    			if(two[i] == '(') ox++;
-    			else ox--;
-    			if(ox < 0)   {
-    				gd = false;
-    				break;
-    			}
-    		}
-    		gd = gd && (ox == 0);
-    		
-    	
-    	    if(fd || gd) cout << "YES\n";
-    	    else cout << "NO\n";
-    	}
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+  int tc;
+  cin >> tc;
+  while (tc--) {
+    string s;
+    cin >> s;
+    int n = s.length();
+    if (s[0] == s[n - 1]) {
+      cout << "NO\n";
+    } else {
+      int cl = 0;
+      int i = 0;
+      int seen = -1;
+      bool corr = true;
+      string one = "";
+      forn(i, n) {
+        if (s[i] == s[0])
+          one.pb('(');
+        else if (s[i] == s[n - 1])
+          one.pb(')');
+        else
+          one.pb('(');
+      }
+
+      string two = "";
+      forn(i, n) {
+        if (s[i] == s[0])
+          two.pb('(');
+        else if (s[i] == s[n - 1])
+          two.pb(')');
+        else
+          two.pb(')');
+      }
+
+      int op = 0;
+      bool fd = true;
+      forn(i, one.size()) {
+        if (one[i] == '(')
+          op++;
+        else
+          op--;
+        if (op < 0) {
+          fd = false;
+          break;
+        }
+      }
+      fd = fd && (op == 0);
+      int ox = 0;
+      bool gd = true;
+      forn(i, one.size()) {
+        if (two[i] == '(')
+          ox++;
+        else
+          ox--;
+        if (ox < 0) {
+          gd = false;
+          break;
+        }
+      }
+      gd = gd && (ox == 0);
+
+      if (fd || gd)
+        cout << "YES\n";
+      else
+        cout << "NO\n";
     }
-    return 0;
+  }
+  return 0;
 }
-
-
