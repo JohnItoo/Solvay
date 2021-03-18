15c5267baf9a5a8774276f6570f20b08d63c465a
JohnOhue <itoohue@gmail.com>
2021-03-18 16:00:01 +0100

    2021-03-18

---
+++ atcoder/arc102_a.cpp
@@ -0,0 +1,89 @@
+// Problem: C - Triangular Relationship
+// Contest: AtCoder - AtCoder Beginner Contest 108
+// URL: https://atcoder.jp/contests/abc108/tasks/arc102_a
+// Memory Limit: 1024 MB
+// Time Limit: 2000 ms
+// 
+// Powered by CP Editor (https://cpeditor.org)
+
+//============================================================================
+// Name        : template.cpp
+// Author      :   $%U%$   
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <bits/stdc++.h>
+using namespace std;
+#define _CRT_SECURE_NO_DEPRECATE  // suppress some compilation warning messages
+                                  // (for VC++ users)
+// Shortcuts for "common" data types in contests
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int, int> ii;
+typedef vector<ii> vii;
+typedef set<int> si;
+typedef map<string, int> msi;
+// To simplify repetitions/loops, Note: define your loop style and stick with
+// it!
+#define x first
+#define y second
+#define pb push_back
+#define mp make_pair
+#define REP(i, a, b) \
+    for (int i = int(a); i <= int(b); i++)  // a to b, and variable i is local!
+#define forn(i, n) for (int i = 0; i < (n); i++)
+#define TRvi(c, it) for (vi::iterator it = (c).begin(); it != (c).end(); it++)
+#define TRvii(c, it) for (vii::iterator it = (c).begin(); it != (c).end(); it++)
+#define TRmsi(c, it) for (msi::iterator it = (c).begin(); it != (c).end(); it++)
+#define INF 2000000000  // 2 billion
+// If you need to recall how to use memset:
+#define MEMSET_INF 127      // about 2B
+#define MEMSET_HALF_INF 63  // about 1B
+// memset(dist, MEMSET_INF, sizeof dist); // useful to initialize shortest path
+// distances memset(dp_memo, -1, sizeof dp_memo); // useful to initialize DP
+// memoization table memset(arr, 0, sizeof arr); // useful to clear array of
+// integers
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int n, k; cin >> n >> k;
+    vi mult;
+    int i = max(2, )
+    while(n * 3 >= i) {
+    	if(i % k == 0) {
+    		mult.pb(i);
+    	}
+    	i++;
+    }
+    int ct = mult.size();
+    i = 0;
+    int j = 0;
+    int val = 1;
+    int ans = 0;
+    REP(i, val, n) {
+    	int targ = i+n;
+        
+        auto lw = lower_bound(mult.begin(), mult.end(), 2*i);
+    	auto it = lower_bound(mult.begin(), mult.end(), targ);
+    	
+    	if(it != mult.end()) {
+    		int lim = *it;
+    		if(lim != targ) --it;
+    		
+    		int idx = it - lw + 1;
+    		cout << *it << " : " << *lw << " : " << idx << endl;
+    		if(*lw == 2*i) {
+    			ans += 1;
+    			ans += 6 * (idx-1);
+    		} else {
+    			ans += 6 * idx;
+    		}
+    	}
+    }
+    cout << ans << endl;
+    
+    return 0;
+}
