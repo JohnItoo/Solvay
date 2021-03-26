7b82fdfe565db376815e33ed1866225d82cdc60f
JohnOhue <itoohue@gmail.com>
2021-03-26 14:00:00 +0100

    2021-03-26

--- codeforces/rounds/CF-1506B.cpp
+++ codeforces/rounds/CF-1506B.cpp
@@ -3,12 +3,12 @@
 // URL: https://codeforces.com/contest/1506/problem/B
 // Memory Limit: 256 MB
 // Time Limit: 2000 ms
-// 
+//
 // Powered by CP Editor (https://cpeditor.org)
 
 //============================================================================
 // Name        : template.cpp
-// Author      :   $%U%$   
+// Author      :   $%U%$
 // Version     :
 // Copyright   : Your copyright notice
 // Description : Hello World in C++, Ansi-style
@@ -32,7 +32,7 @@ typedef map<string, int> msi;
 #define pb push_back
 #define mp make_pair
 #define REP(i, a, b) \
-    for (int i = int(a); i <= int(b); i++)  // a to b, and variable i is local!
+  for (int i = int(a); i <= int(b); i++)  // a to b, and variable i is local!
 #define forn(i, n) for (int i = 0; i < (n); i++)
 #define TRvi(c, it) for (vi::iterator it = (c).begin(); it != (c).end(); it++)
 #define TRvii(c, it) for (vii::iterator it = (c).begin(); it != (c).end(); it++)
@@ -47,35 +47,36 @@ typedef map<string, int> msi;
 // integers
 
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(0);
-    int tc; cin >> tc;
-    while(tc--) {
-    	int n, k; cin >> n >> k;
-    	string s; cin >> s;
-    	vector<int> dp(n+1, 1e9+3);
-    	int idx = -1;
-    	REP(i, 1, n) {
-    		if(s[i-1] == '*' ) {
-    			idx = i;
-    			dp[idx] = 1;
-    			break;
-    		}
-    	}
-    	
-    	REP(i, idx, n) {
-            if(s[i-1] == '.') continue;
-    		for(int j = i-k; j < i; j++) {
-    			if(j < 0) continue;
-    			dp[i] = min(dp[i], dp[j] +1);
-    		}
-    	}
-    	
-    	int en = n-1;
-    	while(s[en] != '*') en--;
-    	cout << dp[en+1] << "\n";
-    	
-    	
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+  int tc;
+  cin >> tc;
+  while (tc--) {
+    int n, k;
+    cin >> n >> k;
+    string s;
+    cin >> s;
+    vector<int> dp(n + 1, 1e9 + 3);
+    int idx = -1;
+    REP(i, 1, n) {
+      if (s[i - 1] == '*') {
+        idx = i;
+        dp[idx] = 1;
+        break;
+      }
     }
-    return 0;
+
+    REP(i, idx, n) {
+      if (s[i - 1] == '.') continue;
+      for (int j = i - k; j < i; j++) {
+        if (j < 0) continue;
+        dp[i] = min(dp[i], dp[j] + 1);
+      }
+    }
+
+    int en = n - 1;
+    while (s[en] != '*') en--;
+    cout << dp[en + 1] << "\n";
+  }
+  return 0;
 }
