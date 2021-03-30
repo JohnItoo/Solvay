f6300b5942ed20ae9190c4362a4625b93cdc9043
JohnOhue <itoohue@gmail.com>
2021-03-30 23:00:00 +0100

    2021-03-30

--- codejam/2021/ReversortEngineering.cpp
+++ codejam/2021/ReversortEngineering.cpp
@@ -33,7 +33,7 @@ typedef map<string, int> msi;
 #define pb push_back
 #define mp make_pair
 #define REP(i, a, b) \
-  for (int i = int(a); i <= int(b); i++)  // a to b, and variable i is local!
+    for (int i = int(a); i <= int(b); i++)  // a to b, and variable i is local!
 #define forn(i, n) for (int i = 0; i < (n); i++)
 #define TRvi(c, it) for (vi::iterator it = (c).begin(); it != (c).end(); it++)
 #define TRvii(c, it) for (vii::iterator it = (c).begin(); it != (c).end(); it++)
@@ -47,39 +47,81 @@ typedef map<string, int> msi;
 // memoization table memset(arr, 0, sizeof arr); // useful to clear array of
 // integers
 
-int main() {
-  ios::sync_with_stdio(false);
-  cin.tie(0);
-  int tc;
-  cin >> tc;
-  int count = 1;
-  while (tc--) {
-    cout << "Case #" << count << ": ";
-    count++;
-    int n, c;
-    cin >> n >> c;
-    if (c < n - 1 || c > n * (n + 1) / 2 - 1) {
-      cout << "IMPOSSIBLE\n";
-    } else {
-      vi a(n);
-      forn(i, n) a[i] = i + 1;
-      for (int i = n - 2; i >= 0; i--) {
-        int end = -1;
-        if (c - i >= n - i) {
-          end = n - 1;
-          reverse(a.begin() + i, a.begin() + end + 1);
-          c -= end - i + 1;
-
-        } else {
-          end = i + (c - i) - 1;
-          reverse(a.begin() + i, a.begin() + end + 1);
-          c -= end - i + 1;
-          break;
-        }
-      }
-      forn(i, n) cout << a[i] << " ";
-      cout << endl;
+void completeSearch() {
+    int n, x;
+    cin >> n >> x;
+    vi a(n);
+    vi sor(n);
+    forn(i, n) {
+        a[i] = i;
+        sor[i] = a[i];
     }
-  }
-  return 0;
+    int cost = 0;
+
+    do {
+        cost = 0;
+        vi sw(n);
+        forn(i, n) sw[i] = a[i];
+
+        REP(i, 0, n - 2) {
+            if (sw[i] == sor[i]) {
+                // cout << "here\n";
+                cost++;
+            } else {
+                int j = 0;
+                while (j < n && sw[j] != sor[i]) j++;
+                //		cout << i << " " << j << " this is j\n";
+                int curr = ((max(j, i) - min(j, i)) + 1);
+                cost += curr;
+                // cout << j << " " << i << " " << curr << endl;
+                reverse(sw.begin() + min(i, j), sw.begin() + max(i, j) + 1);
+                // forn(i,n) cout << a[i] << " ";
+                // cout << endl;
+            }
+        }
+        if (cost == x) break;
+    } while (next_permutation(a.begin(), a.end()));
+    if (cost == x) {
+        forn(i, n) cout << a[i] + 1 << " ";
+        cout << endl;
+    } else {
+        cout << "IMPOSSIBLE\n";
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int tc;
+    cin >> tc;
+    int count = 1;
+    while (tc--) {
+        cout << "Case #" << count << ": ";
+        count++;
+        int n, c;
+        cin >> n >> c;
+        if (c < n - 1 || c > n * (n + 1) / 2 - 1) {
+            cout << "IMPOSSIBLE\n";
+        } else {
+            vi a(n);
+            forn(i, n) a[i] = i + 1;
+            for (int i = n - 2; i >= 0; i--) {
+                int end = -1;
+                if (c - i >= n - i) {
+                    end = n - 1;
+                    reverse(a.begin() + i, a.begin() + end + 1);
+                    c -= end - i + 1;
+
+                } else {
+                    end = i + (c - i) - 1;
+                    reverse(a.begin() + i, a.begin() + end + 1);
+                    c -= end - i + 1;
+                    break;
+                }
+            }
+            forn(i, n) cout << a[i] << " ";
+            cout << endl;
+        }
+    }
+    return 0;
 }
