93d09d6e79b348c04a5c2a16251fa1c024de4736
JohnOhue <itoohue@gmail.com>
2021-04-08 21:00:01 +0100

    2021-04-08

---
+++ codeforces/rounds/CF-1498B.cpp
@@ -0,0 +1,94 @@
+// Problem: B. Box Fitting
+// Contest: Codeforces - CodeCraft-21 and Codeforces Round #711 (Div. 2)
+// URL: https://codeforces.com/contest/1498/problem/B
+// Memory Limit: 256 MB
+// Time Limit: 1000 ms
+// 
+// Powered by CP Editor (https://cpeditor.org)
+
+//============================================================================
+// Name        : template.cpp
+// Author      :   $%U%$   
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <bits/stdc++.h>
+using namespace std;
+#define _CRT_SECURE_NO_DEPRECATE  // suppress some compilation warning messages
+                                  // (for VC++ users)
+// Shortcuts for "common" data types in contests
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int, int> ii;
+typedef vector<ii> vii;
+typedef set<int> si;
+typedef map<string, int> msi;
+// To simplify repetitions/loops, Note: define your loop style and stick with
+// it!
+#define x first
+#define y second
+#define pb push_back
+#define mp make_pair
+#define REP(i, a, b) \
+    for (int i = int(a); i <= int(b); i++)  // a to b, and variable i is local!
+#define forn(i, n) for (int i = 0; i < (n); i++)
+#define TRvi(c, it) for (vi::iterator it = (c).begin(); it != (c).end(); it++)
+#define TRvii(c, it) for (vii::iterator it = (c).begin(); it != (c).end(); it++)
+#define TRmsi(c, it) for (msi::iterator it = (c).begin(); it != (c).end(); it++)
+#define INF 2000000000  // 2 billion
+// If you need to recall how to use memset:
+#define MEMSET_INF 127      // about 2B
+#define MEMSET_HALF_INF 63  // about 1B
+// memset(dist, MEMSET_INF, sizeof dist); // useful to initialize shortest path
+// distances memset(dp_memo, -1, sizeof dp_memo); // useful to initialize DP
+// memoization table memset(arr, 0, sizeof arr); // useful to clear array of
+// integers
+ll W;
+void fill(vector<ll>& here, ll curr) {
+	if(here[0] > curr) {
+		here.pb(W-curr);
+	} else {
+		
+	}
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int tc; cin >> tc;
+    while(tc--) {
+    	int n; ll w; cin >> n >> w;
+    	ll sm = 0;
+    	map<ll, int> fq;
+    	vector<ll> rev;
+    	ll mx = -1;
+    	forn(i, n) {
+    		ll x; cin >> x;
+    		sm += x;
+    		if(!fq.count(x)) {
+    			rev.pb(x);
+    		}
+    		fq[x]++;
+    		mx = max(mx, x);
+    		
+    	}
+    	sort(rev.rbegin(), rev.rend());
+    	vector<ll> here;
+    	here.pb(w);
+    	
+    	int i = 0;
+    	pair<ll, int> bg = {W, 0};
+    	while(i < n) {
+    		
+    	}
+    	
+    	ll y = W / mx;
+    	ll minh = fq[mx] /y;
+    	if(rev.size() > 1 && rev[1] > )
+    
+    	
+    }
+    return 0;
+}
