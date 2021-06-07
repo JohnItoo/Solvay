202aa2eb6ac97e46c80e82f672ceb5e9503f8d98
JohnOhue <itoohue@gmail.com>
2021-06-07 22:21:23 +0100

    2021-06-07

---
+++ codeforces/rounds/CF-1536B.cpp
@@ -0,0 +1,93 @@
+// Problem: B. Prinzessin der Verurteilung
+// Contest: Codeforces - Codeforces Round #724 (Div. 2)
+// URL: https://codeforces.com/contest/1536/problem/B
+// Memory Limit: 256 MB
+// Time Limit: 2000 ms
+//
+// Powered by CP Editor (https://cpeditor.org)
+
+//============================================================================
+// Name        : template.cpp
+// Author      :   $%U%$
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <bits/stdc++.h>
+using namespace std;
+#define _CRT_SECURE_NO_DEPRECATE  // suppress some compilation warning messages
+                                  // (for VC++ users)
+// Shortcuts for "common" data types in contests
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int, int> ii;
+typedef vector<ii> vii;
+typedef set<int> si;
+typedef map<string, int> msi;
+// To simplify repetitions/loops, Note: define your loop style and stick with
+// it!
+#define x first
+#define y second
+#define pb push_back
+#define mp make_pair
+#define REP(i, a, b) \
+  for (int i = int(a); i <= int(b); i++)  // a to b, and variable i is local!
+#define forn(i, n) for (int i = 0; i < (n); i++)
+#define TRvi(c, it) for (vi::iterator it = (c).begin(); it != (c).end(); it++)
+#define TRvii(c, it) for (vii::iterator it = (c).begin(); it != (c).end(); it++)
+#define TRmsi(c, it) for (msi::iterator it = (c).begin(); it != (c).end(); it++)
+#define INF 2000000000  // 2 billion
+// If you need to recall how to use memset:
+#define MEMSET_INF 127      // about 2B
+#define MEMSET_HALF_INF 63  // about 1B
+// memset(dist, MEMSET_INF, sizeof dist); // useful to initialize shortest path
+// distances memset(dp_memo, -1, sizeof dp_memo); // useful to initialize DP
+// memoization table memset(arr, 0, sizeof arr); // useful to clear array of
+// integers
+
+string ans = "";
+void backtrack(int len, string curr, set<string>& st) {
+  if (curr.size() == len) {
+    if (st.find(curr) == st.end() && ans == "") {
+      ans = curr;
+    }
+    return;
+  }
+  forn(i, 26) {
+    string nw = curr;
+    nw.pb('a' + i);
+    backtrack(len, nw, st);
+  }
+}
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+  int tc;
+  cin >> tc;
+  while (tc--) {
+    ans = "";
+    int n;
+    cin >> n;
+    string s;
+    cin >> s;
+
+    set<string> st;
+    forn(i, n) {
+      REP(j, i, n - 1) {
+        int len = j - i + 1;
+        string sub = s.substr(i, len);
+        st.insert(sub);
+      }
+    }
+
+    REP(i, 1, 3) {
+      backtrack(i, "", st);
+      if (ans != "") {
+        cout << ans << endl;
+        break;
+      }
+    }
+  }
+  return 0;
+}
