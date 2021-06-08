de23fe3b7c9d4808f8c9a75ff72081862e126a25
JohnOhue <itoohue@gmail.com>
2021-06-08 23:00:01 +0100

    2021-06-08

---
+++ codeforces/rounds/CF-1536C.cpp
@@ -0,0 +1,92 @@
+// Problem: C. Diluc and Kaeya
+// Contest: Codeforces - Codeforces Round #724 (Div. 2)
+// URL: https://codeforces.com/problemset/problem/1536/C
+// Memory Limit: 256 MB
+// Time Limit: 2000 ms
+// 
+// Powered by CP Editor (https://cpeditor.org)
+
+//============================================================================
+// Name        : template.cpp
+// Author      :   $%U%$   
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <bits/stdc++.h>
+using namespace std;
+#define _CRT_SECURE_NO_DEPRECATE  // suppress some compilation warning messages
+                                  // (for VC++ users)
+// Shortcuts for "common" data types in contests
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int, int> ii;
+typedef vector<ii> vii;
+typedef set<int> si;
+typedef map<string, int> msi;
+// To simplify repetitions/loops, Note: define your loop style and stick with
+// it!
+#define x first
+#define y second
+#define pb push_back
+#define mp make_pair
+#define REP(i, a, b) \
+    for (int i = int(a); i <= int(b); i++)  // a to b, and variable i is local!
+#define forn(i, n) for (int i = 0; i < (n); i++)
+#define TRvi(c, it) for (vi::iterator it = (c).begin(); it != (c).end(); it++)
+#define TRvii(c, it) for (vii::iterator it = (c).begin(); it != (c).end(); it++)
+#define TRmsi(c, it) for (msi::iterator it = (c).begin(); it != (c).end(); it++)
+#define INF 2000000000  // 2 billion
+// If you need to recall how to use memset:
+#define MEMSET_INF 127      // about 2B
+#define MEMSET_HALF_INF 63  // about 1B
+// memset(dist, MEMSET_INF, sizeof dist); // useful to initialize shortest path
+// distances memset(dp_memo, -1, sizeof dp_memo); // useful to initialize DP
+// memoization table memset(arr, 0, sizeof arr); // useful to clear array of
+// integers
+int gcd(int a, int b) {
+	if(b ==0) return a;
+	return gcd(b, a%b);
+}
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int tc; cin >> tc;
+    while(tc--) {
+    	int n; cin >> n;
+    	string s; cin >> s;
+    	int d = 0, k = 0;
+    	map<ii, int> ct;
+    	forn(i, n) {
+    		if(s[i] == 'D') d++;
+    		else k++;
+    		if(d == 0 || k == 0) {
+    			cout << d + k << " ";
+    		} else if(d == k) {
+    			ct[{1, 1}]++;
+    			cout <<ct[{1,1}] << " ";
+    			
+    		} else {
+    			int gg = gcd(d, k);
+    			// cout << endl;//
+    			// cout << d << " d :  k : gg " << k << " " << gg << endl;
+    			d/=gg; k/=gg;
+    			char cd = d + '0';
+    			char ck = k + '0';
+    			
+    			ct[{d, k}]++;
+    			// if(d % k != 0) {
+    				// cout << ct[s] 
+    			// } else {
+    				// ct[d/k]++;
+    				// cout << ct[d/k] << " ";
+    			// }
+    			cout << ct[{d, k}] << " ";
+    		}		
+    	}
+    	cout << endl;
+    	
+    }
+    return 0;
+}
