54ecc42c7082e9a3ecf0ec4fb292d4e00fc263d6
JohnOhue <itoohue@gmail.com>
2021-08-28 21:00:00 +0100

    2021-08-28

---
+++ atcoder/ABC-117D.cpp
@@ -0,0 +1,76 @@
+// Problem: D - XXOR
+// Contest: AtCoder - AtCoder Beginner Contest 117
+// URL: https://atcoder.jp/contests/abc117/tasks/abc117_d
+// Memory Limit: 1024 MB
+// Time Limit: 2000 ms
+// 
+// Powered by CP Editor (https://cpeditor.org)
+
+//============================================================================
+// Name        : template.cpp
+// Author      :   $%U%$   
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <bits/stdc++.h>
+using namespace std;
+#define _CRT_SECURE_NO_DEPRECATE  // suppress some compilation warning messages
+                                  // (for VC++ users)
+// Shortcuts for "common" data types in contests
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int, int> ii;
+typedef vector<ii> vii;
+typedef set<int> si;
+typedef map<string, int> msi;
+// To simplify repetitions/loops, Note: define your loop style and stick with
+// it!
+#define x first
+#define y second
+#define pb push_back
+#define mp make_pair
+#define REP(i, a, b) \
+    for (int i = int(a); i <= int(b); i++)  // a to b, and variable i is local!
+#define forn(i, n) for (int i = 0; i < (n); i++)
+#define TRvi(c, it) for (vi::iterator it = (c).begin(); it != (c).end(); it++)
+#define TRvii(c, it) for (vii::iterator it = (c).begin(); it != (c).end(); it++)
+#define TRmsi(c, it) for (msi::iterator it = (c).begin(); it != (c).end(); it++)
+#define INF 2000000000  // 2 billion
+// If you need to recall how to use memset:
+#define MEMSET_INF 127      // about 2B
+#define MEMSET_HALF_INF 63  // about 1B
+// memset(dist, MEMSET_INF, sizeof dist); // useful to initialize shortest path
+// distances memset(dp_memo, -1, sizeof dp_memo); // useful to initialize DP
+// memoization table memset(arr, 0, sizeof arr); // useful to clear array of
+// integers
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int n; ll k; cin >> n >> k;
+    vector<ll> a(n);
+    forn(i, n) {
+    	cin >> a[i];
+    }
+    uint64_t curr  = 0;
+    for(int t = 62; t >= 0; t--) {
+    	uint64_t  h = (1ULL << t);
+
+    	if(h > k - curr) continue;
+  
+        ll sm = 0;
+        forn(i, n) {
+        	if(a[i] & h) sm -= h;
+        	else sm += h;
+        }
+        if(sm > 0) {curr += h;}
+    }
+    ll res = 0;
+    forn(i, n) {
+    	res += (curr ^ a[i]);
+    }
+    cout << res <<endl;
+    return 0;
+}
