7e345a613315a20873533482529c5e8364d653c3
JohnOhue <itoohue@gmail.com>
2021-09-14 12:00:00 +0100

    2021-09-14

---
+++ codeforces/rounds/CF-1566D2.cpp
@@ -0,0 +1,81 @@
+// Problem: D1. Seating Arrangements (easy version)
+// Contest: Codeforces - Codeforces Global Round 16
+// URL: https://codeforces.com/contest/1566/problem/D1
+// Memory Limit: 256 MB
+// Time Limit: 1000 ms
+//
+// Powered by CP Editor (https://cpeditor.org)
+
+//============================================================================
+// Name        : template.cpp
+// Author      :   $%U%$
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <bits/stdc++.h>
+using namespace std;
+#define _CRT_SECURE_NO_DEPRECATE  // suppress some compilation warning messages
+                                  // (for VC++ users)
+// Shortcuts for "common" data types in contests
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int, int> ii;
+typedef vector<ii> vii;
+typedef set<int> si;
+typedef map<string, int> msi;
+// To simplify repetitions/loops, Note: define your loop style and stick with
+// it!
+#define x first
+#define y second
+#define pb push_back
+#define mp make_pair
+#define REP(i, a, b) \
+  for (int i = int(a); i <= int(b); i++)  // a to b, and variable i is local!
+#define forn(i, n) for (int i = 0; i < (n); i++)
+#define TRvi(c, it) for (vi::iterator it = (c).begin(); it != (c).end(); it++)
+#define TRvii(c, it) for (vii::iterator it = (c).begin(); it != (c).end(); it++)
+#define TRmsi(c, it) for (msi::iterator it = (c).begin(); it != (c).end(); it++)
+#define INF 2000000000  // 2 billion
+// If you need to recall how to use memset:
+#define MEMSET_INF 127      // about 2B
+#define MEMSET_HALF_INF 63  // about 1B
+// memset(dist, MEMSET_INF, sizeof dist); // useful to initialize shortest path
+// distances memset(dp_memo, -1, sizeof dp_memo); // useful to initialize DP
+// memoization table memset(arr, 0, sizeof arr); // useful to clear array of
+// integers
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+  int tc;
+  cin >> tc;
+  while (tc--) {
+    int n, m;
+    cin >> n >> m;
+    vii vals;
+    map<int, int> fq;
+    REP(i, 1, n * m) {
+      int x;
+      cin >> x;
+      vals.pb({x, i});
+      fq[x]++;
+    }
+    sort(vals.begin(), vals.end());
+
+    forn(i, n * m) { vals[i].y = -vals[i].y; }
+
+    int ans = 0;
+    forn(i, n) {
+      sort(vals.begin() + (i * m), vals.begin() + (i * m + m));
+      forn(j, m) {
+        REP(k, 0, j - 1) {
+          if (vals[i * m + k].y > vals[i * m + j].y) ans++;
+        }
+      }
+    }
+    cout << ans << "\n";
+  }
+  return 0;
+}
