99c8ae71c7d65ce0ceaf110ede9edcce00e51272
John Ohue <itoohue@gmail.com>
2021-10-13 19:00:00 +0100

    2021-10-13

---
+++ codeforces/rounds/CF-1593B.cpp
@@ -0,0 +1,104 @@
+// Problem: B. Make it Divisible by 25
+// Contest: Codeforces - Codeforces Round #748 (Div. 3)
+// URL: https://codeforces.com/contest/1593/problem/B
+// Memory Limit: 256 MB
+// Time Limit: 1000 ms
+// 
+// Powered by CP Editor (https://cpeditor.org)
+
+//============================================================================
+// Name        : BXK
+// Author      : Template
+// Version     :
+// Copyright   : Who was here
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+#include <algorithm>
+#include <set>
+#include <map>
+#include <set>
+#include <vector>
+#include <string.h> // for memset in CF judge.
+using namespace std;
+#define _CRT_SECURE_NO_DEPRECATE // suppress some compilation warning messages (for VC++ users)
+// Shortcuts for "common" data types in contests
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int, int> ii;
+typedef vector<ii> vii;
+typedef set<int> si;
+typedef map<string, int> msi;
+// To simplify repetitions/loops, Note: define your loop style and stick with it!
+#define x first
+#define y second
+#define pb push_back
+#define mp make_pair
+#define REP(i, a, b) \
+for (int i = int(a); i <= int(b); i++) // a to b, and variable i is local!
+#define forn(i,n) \
+for (int i =0; i<(n); i++)
+#define TRvi(c, it) \
+for (vi::iterator it = (c).begin(); it != (c).end(); it++)
+#define TRvii(c, it) \
+for (vii::iterator it = (c).begin(); it != (c).end(); it++)
+#define TRmsi(c, it) \
+for (msi::iterator it = (c).begin(); it != (c).end(); it++)
+#define INF 2000000000 // 2 billion
+// If you need to recall how to use memset:
+#define MEMSET_INF 127 // about 2B
+#define MEMSET_HALF_INF 63 // about 1B
+//memset(dist, MEMSET_INF, sizeof dist); // useful to initialize shortest path distances
+//memset(dp_memo, -1, sizeof dp_memo); // useful to initialize DP memoization table
+//memset(arr, 0, sizeof arr); // useful to clear array of integers
+void solve() {
+   string s; cin >> s;
+  
+   int n = s.size();
+   int curr = INF;
+   
+   for(int i = n-1; i >= 0; i--) {
+   
+   	 if(s[i] == '0') {
+   	 		int take = n-1 - i;
+   	 	for(int j = i-1; j>= 0; j--) {
+   	 		if(s[j] == '5' || s[j] == '0') {
+   	 			int diff = i - j - 1;
+   	 			curr = min(curr, diff + take);
+   	 			break;
+   	 		}
+   	 	}
+   	 }
+   }
+   
+   for(int i = n-1; i >= 0; i--) {
+   
+   	 if(s[i] == '5') {
+   	 		int take = n-1 - i;
+   	 	for(int j = i-1; j>= 0; j--) {
+   	 		if(s[j] == '7' || s[j] == '2') {
+   	 			int diff = i - j - 1;
+   	 			curr = min(curr, diff + take);
+   	 			break;
+   	 		}
+   	 	}
+   	 }
+   }
+   cout << curr << endl;
+}
+
+int main() {
+ios::sync_with_stdio(false);
+cin.tie(0);
+int tc = 1;
+cin >> tc;
+int count = 1;
+while(tc--) {
+
+ //cout << "Case #" << count << ": "; //CODEJAM/KICKSTART
+ count++;
+ solve();
+}
+return 0;
+}
