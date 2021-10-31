2a356f248869e09c53a5cb2e89d6f7f4ab78a0d1
John Ohue <itoohue@gmail.com>
2021-10-31 11:00:00 +0000

    2021-10-31

---
+++ cses/CF-1604B.cpp
@@ -0,0 +1,87 @@
+// Problem: B. XOR Specia-LIS-t
+// Contest: Codeforces - Codeforces Round #752 (Div. 2)
+// URL: https://codeforces.com/contest/1604/problem/B
+// Memory Limit: 256 MB
+// Time Limit: 1000 ms
+//
+// Powered by CP Editor (https://cpeditor.org)
+
+//============================================================================
+// Name        : BXK
+// Author      : Template
+// Version     :
+// Copyright   : Who was here
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+#include <bits/stdc++.h>
+using namespace std;
+#define _CRT_SECURE_NO_DEPRECATE  // suppress some compilation warning messages
+                                  // (for VC++ users)
+// Shortcuts for "common" data types in contests
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int, int> ii;
+typedef vector<ii> vii;
+typedef set<int> si;
+typedef map<string, int> msi;
+// To simplify repetitions/loops, Note: define your loop style and stick with
+// it!
+#define x first
+#define y second
+#define pb push_back
+#define mp make_pair
+#define REP(i, a, b) \
+  for (int i = int(a); i <= int(b); i++)  // a to b, and variable i is local!
+#define forn(i, n) for (int i = 0; i < (n); i++)
+#define TRvi(c, it) for (vi::iterator it = (c).begin(); it != (c).end(); it++)
+#define TRvii(c, it) for (vii::iterator it = (c).begin(); it != (c).end(); it++)
+#define TRmsi(c, it) for (msi::iterator it = (c).begin(); it != (c).end(); it++)
+#define INF 2000000000  // 2 billion
+// If you need to recall how to use memset:
+#define MEMSET_INF 127      // about 2B
+#define MEMSET_HALF_INF 63  // about 1B
+// memset(dist, MEMSET_INF, sizeof dist); // useful to initialize shortest path
+// distances memset(dp_memo, -1, sizeof dp_memo); // useful to initialize DP
+// memoization table memset(arr, 0, sizeof arr); // useful to clear array of
+// integers
+void solve() {
+  int n;
+  cin >> n;
+  vi a(n);
+  forn(i, n) cin >> a[i];
+
+  if (n % 2 == 0) {
+    cout << "YES\n";
+    return;
+  }
+
+  bool dec = false;
+
+  int ls = 0;
+
+  forn(i, n - 1) {
+    if (a[i] >= a[i + 1]) {
+      dec = true;
+    }
+  }
+
+  if (dec) {
+    cout << "YES\n";
+  } else {
+    cout << "NO\n";  // maybe another way
+  }
+}
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+  int tc = 1;
+  cin >> tc;
+  int count = 1;
+  while (tc--) {
+    // cout << "Case #" << count << ": "; //CODEJAM/KICKSTART
+    count++;
+    solve();
+  }
+  return 0;
+}
