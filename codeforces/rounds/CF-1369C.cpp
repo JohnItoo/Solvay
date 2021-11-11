9b53533b9e6340ac286ea152b827ff4207da9c53
John Ohue <itoohue@gmail.com>
2021-11-11 22:00:00 +0000

    2021-11-11

--- codeforces/rounds/CF-1369C.cpp
+++ codeforces/rounds/CF-1369C.cpp
@@ -1,20 +1,22 @@
+// Problem: F. RationalLee
+// Contest: Codeforces - Greedy #1
+// URL: https://codeforces.com/group/pcTovAeVHg/contest/338449/problem/F
+// Memory Limit: 256 MB
+// Time Limit: 2000 ms
+//
+// Powered by CP Editor (https://cpeditor.org)
+
 //============================================================================
-// Name        : template.cpp
-// Author      :
+// Name        : BXK
+// Author      : Template
 // Version     :
-// Copyright   : Your copyright notice
+// Copyright   : Who was here
 // Description : Hello World in C++, Ansi-style
 //============================================================================
-
-#include <iostream>
-#include <algorithm>
-#include <set>
-#include <map>
-#include <set>
-#include <vector>
-#include <string.h> // for memset in CF judge.
+#include <bits/stdc++.h>
 using namespace std;
-#define _CRT_SECURE_NO_DEPRECATE // suppress some compilation warning messages (for VC++ users)
+#define _CRT_SECURE_NO_DEPRECATE  // suppress some compilation warning messages
+                                  // (for VC++ users)
 // Shortcuts for "common" data types in contests
 typedef long long ll;
 typedef vector<int> vi;
@@ -22,69 +24,76 @@ typedef pair<int, int> ii;
 typedef vector<ii> vii;
 typedef set<int> si;
 typedef map<string, int> msi;
-// To simplify repetitions/loops, Note: define your loop style and stick with it!
+// To simplify repetitions/loops, Note: define your loop style and stick with
+// it!
 #define x first
 #define y second
 #define pb push_back
 #define mp make_pair
 #define REP(i, a, b) \
-for (int i = int(a); i <= int(b); i++) // a to b, and variable i is local!
-#define forn(i,n) \
-for (int i =0; i<(n); i++)
-#define TRvi(c, it) \
-for (vi::iterator it = (c).begin(); it != (c).end(); it++)
-#define TRvii(c, it) \
-for (vii::iterator it = (c).begin(); it != (c).end(); it++)
-#define TRmsi(c, it) \
-for (msi::iterator it = (c).begin(); it != (c).end(); it++)
-#define INF 2000000000 // 2 billion
+  for (int i = int(a); i <= int(b); i++)  // a to b, and variable i is local!
+#define forn(i, n) for (int i = 0; i < (n); i++)
+#define TRvi(c, it) for (vi::iterator it = (c).begin(); it != (c).end(); it++)
+#define TRvii(c, it) for (vii::iterator it = (c).begin(); it != (c).end(); it++)
+#define TRmsi(c, it) for (msi::iterator it = (c).begin(); it != (c).end(); it++)
+#define INF 2000000000  // 2 billion
 // If you need to recall how to use memset:
-#define MEMSET_INF 127 // about 2B
-#define MEMSET_HALF_INF 63 // about 1B
-//memset(dist, MEMSET_INF, sizeof dist); // useful to initialize shortest path distances
-//memset(dp_memo, -1, sizeof dp_memo); // useful to initialize DP memoization table
-//memset(arr, 0, sizeof arr); // useful to clear array of integers
+#define MEMSET_INF 127      // about 2B
+#define MEMSET_HALF_INF 63  // about 1B
+// memset(dist, MEMSET_INF, sizeof dist); // useful to initialize shortest path
+// distances memset(dp_memo, -1, sizeof dp_memo); // useful to initialize DP
+// memoization table memset(arr, 0, sizeof arr); // useful to clear array of
+// integers
+void solve() {
+  ll n, k;
+  cin >> n >> k;
+  vector<ll> a(n);
+  forn(i, n) cin >> a[i];
+  sort(a.begin(), a.end());
+  int ones = 0;
+  vector<int> ct;
+
+  forn(i, k) {
+    int x;
+    cin >> x;
+    if (x == 1)
+      ones++;
+    else
+      ct.pb(x);
+  }
+  sort(ct.rbegin(), ct.rend());
+
+  int i = n - 1;
+  int j = 0;
+  ll ans = 0;
+  while (ones > 0) {
+    ans += (2 * a[i--]);
+    ones--;
+  }
+  forn(l, ct.size()) {
+    int hr = ct[l];
+    ans += a[i];
+    hr--;
+
+    ans += a[j];
+    j += hr;
+
+    i--;
+  }
+
+  cout << ans << endl;
+}
 
 int main() {
-	ios::sync_with_stdio(false);
-	cin.tie(0);
-
-	int t; cin >> t;
-	while (t--) {
-		int n, k; cin >> n >> k;
-		ll a[n];
-		forn(i, n) cin >> a[i];
-		// vector<pair<ll, ll> > w;
-		ll w[k];
-		map<ll, ll> mx;
-		forn(i, k) {
-			ll yy; cin >> yy;
-			// w.pb(mp(yy, i));
-			// mx[i] = yy;
-			w[i] == yy;
-		}
-		sort(a, a + n);
-		sort(w, w + k);
-
-		int rt = n - 1;
-		int lt = 0;
-		ll ans = 0;
-		forn(i, k) {
-			ll ft = w[i];
-
-			// ll ft = w[i].first;
-			// ll sec = w[i].second;
-			ans += a[rt];
-			if (ft > 1) {
-				ans += a[lt++];
-			} else {
-				ans += a[rt];
-			}
-			rt--;
-		}
-		cout << ans << endl;
-
-
-	}
-	return 0;
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+  int tc = 1;
+  cin >> tc;
+  int count = 1;
+  while (tc--) {
+    // cout << "Case #" << count << ": "; //CODEJAM/KICKSTART
+    count++;
+    solve();
+  }
+  return 0;
 }
