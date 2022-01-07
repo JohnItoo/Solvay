498fc671d0e5a4854f87b7b51e53e717835484c2
John Ohue <itoohue@gmail.com>
2022-01-07 19:00:00 +0000

    2022-01-07

---
+++ cses/DigitQueries.cpp
@@ -0,0 +1,118 @@
+// Problem: Digit Queries
+// Contest: CSES - CSES Problem Set
+// URL: https://cses.fi/problemset/task/2431
+// Memory Limit: 512 MB
+// Time Limit: 1000 ms
+// 
+// Powered by CP Editor (https://cpeditor.org)
+
+//============================================================================
+// Name        : BXK
+// Author      : Template
+// Version     :
+// Copyright   : Who was here
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+#include <bits/stdc++.h>
+using namespace std;
+#define _CRT_SECURE_NO_DEPRECATE // suppress some compilation warning messages (for VC++ users)
+// Shortcuts for "common" data types in contests
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int, int> ii;
+typedef vector<ii> vii;
+typedef set<int> si;
+typedef map<string, int> msi;
+// To simplify repetitions/loops, Note: define your loop style and stick with it!
+#define x first
+#define y second
+#define pb push_back
+#define mp make_pair
+#define REP(i, a, b) \
+for (int i = int(a); i <= int(b); i++) // a to b, and variable i is local!
+#define forn(i,n) \
+for (int i =0; i<(n); i++)
+#define TRvi(c, it) \
+for (vi::iterator it = (c).begin(); it != (c).end(); it++)
+#define TRvii(c, it) \
+for (vii::iterator it = (c).begin(); it != (c).end(); it++)
+#define TRmsi(c, it) \
+for (msi::iterator it = (c).begin(); it != (c).end(); it++)
+#define INF 2000000000 // 2 billion
+// If you need to recall how to use memset:
+#define MEMSET_INF 127 // about 2B
+#define MEMSET_HALF_INF 63 // about 1B
+//memset(dist, MEMSET_INF, sizeof dist); // useful to initialize shortest path distances
+//memset(dp_memo, -1, sizeof dp_memo); // useful to initialize DP memoization table
+//memset(arr, 0, sizeof arr); // useful to clear array of integers
+vector<ll> vals;
+vector<ll> smvals;
+void solve() {
+ ll k; cin >> k;
+ ll sm = 0;
+ int i = 0;
+ 
+ while(smvals[i] < k) {
+    i++;
+ }
+ 
+ ll pos = (k - smvals[i-1] - 1) / i;
+ ll idx = (k - smvals[i-1] - 1) % i;
+ 
+ ll pw = 1; int cur = 1;
+ while(cur <  i) {
+ 	cur++;
+ 	pw *= 10;
+
+ }
+ 
+ ll num = pw + pos;
+ //num--;
+ //cout << num << " ";
+ int ct = i-1;
+ while(num > 0 && ct > idx+1) {
+ 	num /= 10;
+ 	ct--;
+ }
+ cout << num % 10 <<endl;
+ //cout << pw << " "<<num % 10 <<endl;
+  
+ 
+}
+
+int main() {
+ios::sync_with_stdio(false);
+cin.tie(0);
+int tc = 1;
+cin >> tc;
+int count = 1;
+
+vals.pb(0);
+smvals.pb(0);
+vals.pb(9);
+smvals.pb(9);
+vals.pb(90);
+smvals.pb(90);
+
+ll sm = 90 + 9;
+ll mx = 1e18;
+while(sm < mx && vals.size() <= 19) {
+	ll nxt = (vals[vals.size() - 1] * 10LL);
+	sm += nxt;
+	//cout << nxt << " " << vals.size() <<endl;
+	vals.pb(nxt);
+	smvals.pb(sm);
+	
+}
+// forn(i, vals.size()) {
+	// cout << vals[i] << " ";
+// }
+//return 0;
+while(tc--) {
+
+ //cout << "Case #" << count << ": "; //CODEJAM/KICKSTART
+ count++;
+ solve();
+}
+return 0;
+}
